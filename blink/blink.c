6f7db94b2c67527250754aaac6a14dffb01c5614
Dhruva Gole <41233856+DhruvaG2000@users.noreply.github.com>
2021-02-02 13:27:58 +0530

    added LED ON and OFF serial output

--- blink/blink.c
+++ blink/blink.c
@@ -5,15 +5,20 @@
  */
 
 #include "pico/stdlib.h"
+#include <stdio.h>
 
 int main() {
+    stdio_init_all();
     const uint LED_PIN = 25;
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
     while (true) {
+        printf("LED ON!\n");
         gpio_put(LED_PIN, 1);
         sleep_ms(250);
+        printf("LED OFF!\n");
         gpio_put(LED_PIN, 0);
         sleep_ms(250);
     }
 }
+
