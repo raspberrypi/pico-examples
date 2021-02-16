67975d1380692379eff4725b41311f6cb41030d3
Luke Wren <wren6991@gmail.com>
2021-02-16 13:53:27 +0000

    hello_double_tap: use pico_bootsel_via_double_reset from SDK

--- system/hello_double_tap/hello_double_tap.c
+++ system/hello_double_tap/hello_double_tap.c
@@ -6,10 +6,14 @@
 
 #include "pico/stdlib.h"
 
-// This is a regular old LED blinking example, however it is linked with double_tap_usb_boot
-// so pressing reset quickly twice, will reset into USB bootloader
+// This is a regular old LED blinking example, however it is linked with the
+// `pico_bootsel_via_double_reset` library, so resetting the board twice
+// quickly will enter the USB bootloader. This is useful for boards which have
+// a reset button but no BOOTSEL, as long as you remember to always link the
+// `pico_bootsel_via_double_reset` library!
+
 int main() {
-    const uint LED_PIN = 21;
+    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
     while (true) {
