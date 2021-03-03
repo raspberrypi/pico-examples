ded0d8276d15b8f7364275ff0c0bb8ca44b85f10
Felipe Kuhne <fkuhne@dbserver.com.br>
2021-03-03 17:27:11 -0300

    Added onboard_temperature example.

---
+++ adc/onboard_temperature/onboard_temperature.c
@@ -0,0 +1,59 @@
+/**
+ * Copyright (c) 2021 Raspberry Pi (Trading) Ltd.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ */
+
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/adc.h"
+
+/* Choose 'C' for Celsius or 'F' for Fahrenheit. */
+#define TEMPERATURE_UNITS 'C'
+
+/* References for this implementation:
+ * raspberry-pi-pico-c-sdk.pdf, Section '4.1.1. hardware_adc'
+ * pico-examples/adc/adc_console/adc_console.c */
+const float read_onboard_temperature(const char unit) {
+    
+    /* 12-bit conversion, assume max value == ADC_VREF == 3.3 V */
+    const float conversionFactor = 3.3f / (1 << 12);
+
+    float adc = adc_read() * conversionFactor;
+    float tempC = 27.0 - (adc - 0.706) / 0.001721;
+
+    if (unit == 'C') {
+        return tempC;
+    } else if (unit == 'F') {
+        return tempC * 9 / 5 + 32;
+    }
+
+    return -1.0;
+}
+
+int main() {
+    stdio_init_all();
+    gpio_init(PICO_DEFAULT_LED_PIN);
+    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+
+    /* Initialize hardware AD converter, enable onboard temperature sensor and
+     *   select its channel (do this once for efficiency, but beware that this
+     *   is a global operation). */
+    adc_init();
+    adc_set_temp_sensor_enabled(true);
+    adc_select_input(4);
+
+    while (true) {
+        float temperature = read_onboard_temperature(TEMPERATURE_UNITS);
+        printf("Onboard temperature = %.02f %c\n", temperature, TEMPERATURE_UNITS);
+
+        gpio_put(PICO_DEFAULT_LED_PIN, 1);
+        sleep_ms(10);
+
+        gpio_put(PICO_DEFAULT_LED_PIN, 0);
+        sleep_ms(990);
+    }
+
+    return 0;
+}
