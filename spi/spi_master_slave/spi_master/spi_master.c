ca3f6d6c71930dae89d961d3c3497d032f062d59
Michael Stoops <spam@michaelstoops.com>
2021-04-13 21:05:08 -0700

    Minor changes to spi_master and spi_slave. Adjusted documentation.

--- spi/spi_master_slave/spi_master/spi_master.c
+++ spi/spi_master_slave/spi_master/spi_master.c
@@ -1,10 +1,26 @@
-/**
- * Copyright (c) 2021 Michael Stoops. All rights reserved.
- * Portions copyright (c) 2021 Raspberry Pi (Trading) Ltd.
- *
- * SPDX-License-Identifier: BSD-3-Clause
- */
-
+// Copyright (c) 2021 Michael Stoops. All rights reserved.
+// Portions copyright (c) 2021 Raspberry Pi (Trading) Ltd.
+// 
+// Redistribution and use in source and binary forms, with or without modification, are permitted provided that the 
+// following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+//    disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
+//    following disclaimer in the documentation and/or other materials provided with the distribution.
+// 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote
+//    products derived from this software without specific prior written permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
+// INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE 
+// USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// SPDX-License-Identifier: BSD-3-Clause
+//
 // Example of an SPI bus master using the PL022 SPI interface
 
 #include <stdio.h>
@@ -39,8 +55,8 @@ int main() {
 
     printf("SPI master example\n");
 
-    // Enable SPI 0 at 1 KHz and connect to GPIOs
-    spi_init(spi_default, 1000);
+    // Enable SPI 0 at 1 MHz and connect to GPIOs
+    spi_init(spi_default, 1000 * 1000);
     gpio_set_function(PICO_DEFAULT_SPI_RX_PIN, GPIO_FUNC_SPI);
     gpio_set_function(PICO_DEFAULT_SPI_SCK_PIN, GPIO_FUNC_SPI);
     gpio_set_function(PICO_DEFAULT_SPI_TX_PIN, GPIO_FUNC_SPI);
@@ -48,25 +64,26 @@ int main() {
     // Make the SPI pins available to picotool
     bi_decl(bi_4pins_with_func(PICO_DEFAULT_SPI_RX_PIN, PICO_DEFAULT_SPI_TX_PIN, PICO_DEFAULT_SPI_SCK_PIN, PICO_DEFAULT_SPI_CSN_PIN, GPIO_FUNC_SPI));
 
-    printf("SPI master initialised.\n");
-
     uint8_t out_buf[BUF_LEN], in_buf[BUF_LEN];
 
     // Initialize output buffer
     for (size_t i = 0; i < BUF_LEN; ++i) {
         out_buf[i] = i;
     }
-    
-    printf("The following buffer will be written to MOSI endlessly.\n");
+
+    printf("SPI master says: The following buffer will be written to MOSI endlessly:\n");
     printbuf(out_buf, BUF_LEN);
 
-    for (size_t i = 0; true; ++i) {
+    for (size_t i = 0; ; ++i) {
         // Write the output buffer to MOSI, and at the same time read from MISO.
         spi_write_read_blocking(spi_default, out_buf, in_buf, BUF_LEN);
 
         // Write to stdio whatever came in on the MISO line.
-        printf("Read the following from MISO, page %d:\n", i);
+        printf("SPI master says: read page %d from the MISO line:\n", i);
         printbuf(in_buf, BUF_LEN);
+
+        // Sleep for ten seconds so you get a chance to read the output.
+        sleep_ms(10 * 1000);
     }
 #endif
 }
--- spi/spi_master_slave/spi_slave/spi_slave.c
+++ spi/spi_master_slave/spi_slave/spi_slave.c
@@ -1,10 +1,26 @@
-/**
- * Copyright (c) 2021 Michael Stoops. All rights reserved.
- * Portions copyright (c) 2021 Raspberry Pi (Trading) Ltd.
- *
- * SPDX-License-Identifier: BSD-3-Clause
- */
-
+// Copyright (c) 2021 Michael Stoops. All rights reserved.
+// Portions copyright (c) 2021 Raspberry Pi (Trading) Ltd.
+// 
+// Redistribution and use in source and binary forms, with or without modification, are permitted provided that the 
+// following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+//    disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
+//    following disclaimer in the documentation and/or other materials provided with the distribution.
+// 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote
+//    products derived from this software without specific prior written permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
+// INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE 
+// USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// SPDX-License-Identifier: BSD-3-Clause
+//
 // Example of an SPI bus slave using the PL022 SPI interface
 
 #include <stdio.h>
@@ -41,8 +57,8 @@ int main() {
 
     printf("SPI slave example\n");
 
-    // Enable SPI 0 at 1 KHz and connect to GPIOs
-    spi_init(spi_default, 1000);
+    // Enable SPI 0 at 1 MHz and connect to GPIOs
+    spi_init(spi_default, 1000 * 1000);
     spi_set_slave(spi_default, true);
     gpio_set_function(PICO_DEFAULT_SPI_RX_PIN, GPIO_FUNC_SPI);
     gpio_set_function(PICO_DEFAULT_SPI_SCK_PIN, GPIO_FUNC_SPI);
@@ -50,8 +66,6 @@ int main() {
     gpio_set_function(PICO_DEFAULT_SPI_CSN_PIN, GPIO_FUNC_SPI);
     // Make the SPI pins available to picotool
     bi_decl(bi_4pins_with_func(PICO_DEFAULT_SPI_RX_PIN, PICO_DEFAULT_SPI_TX_PIN, PICO_DEFAULT_SPI_SCK_PIN, PICO_DEFAULT_SPI_CSN_PIN, GPIO_FUNC_SPI));
-
-    printf("SPI slave initialised.\n");
 
     uint8_t out_buf[BUF_LEN], in_buf[BUF_LEN];
 
@@ -61,14 +75,16 @@ int main() {
         out_buf[i] = ~i;
     }
 
-    printf("When reading from MOSI, the following buffer will be written to MISO:\n");
+    printf("SPI slave says: When reading from MOSI, the following buffer will be written to MISO:\n");
     printbuf(out_buf, BUF_LEN);
     
-    for (size_t i = 0; true; ++i) {
+    for (size_t i = 0; ; ++i) {
         // Write the output buffer to MISO, and at the same time read from MOSI.
         spi_write_read_blocking(spi_default, out_buf, in_buf, BUF_LEN);
 
-        // The slave doesn't print the buffer. It can't afford to ignore the SPI bus while writing to stdio.
+        // Write to stdio whatever came in on the MOSI line.
+        printf("SPI slave says: read page %d from the MOSI line:\n", i);
+        printbuf(in_buf, BUF_LEN);
     }
 #endif
 }
