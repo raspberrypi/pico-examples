4d02c2ff5d8b09661541ac38e9dd912904cbaa14
matiasilva <matias.silva@raspberrypi.com>
2021-06-29 14:05:12 +0100

    Bootstrap OLED example

---
+++ i2c/oled_i2c/oled_i2c.c
@@ -0,0 +1,10 @@
+/**
+ * Copyright (c) 2021 Raspberry Pi (Trading) Ltd.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "pico/binary_info.h"
+#include "hardware/i2c.h"
