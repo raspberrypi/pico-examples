0a76daad2852f94992cda685055de62385050b54
SEEK-THE-VOID-LIGHT <ian55139819@gmail.com>
2021-07-12 18:51:25 +0200

    typo

--- pwm/servo/main.c
+++ pwm/servo/main.c
@@ -4,16 +4,13 @@
 
 int main()
 {
+    servo_init(1, 50);
     for(;;)
     {
-        servo_init(1, 50);
         servo_put(1, 0, true);
-
         sleep_ms(1000);
 
-        servo_init(1, 50);
         servo_put(1, 90, true);
-
         sleep_ms(1000);
     }
 }
