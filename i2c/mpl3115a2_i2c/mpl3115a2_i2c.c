6b0b5b67c6185672b7e3dc693518bf3e28075381
matiasilva <matias.silva@raspberrypi.com>
2021-07-12 09:59:50 +0100

    Bootstrap example

---
+++ i2c/mpl3115a2_i2c/mpl3115a2_i2c.c
@@ -0,0 +1,9 @@
+/**
+ * Copyright (c) 2021 Raspberry Pi (Trading) Ltd.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include "pico/stdlib.h"
+#include "pico/binary_info.h"
+#include "hardware/i2c.h"
