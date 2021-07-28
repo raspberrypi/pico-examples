ddd4e2d7cf35874ed28bcdd9c2a7a6ad20b2d578
Aayushi Jhaveri <aayushi.jhaveri@raspberrypi.com>
2021-07-28 15:52:53 +0100

    Minor changes to code + documentation

--- ethernet/w5500_spi/w5500_spi.c
+++ ethernet/w5500_spi/w5500_spi.c
@@ -40,9 +40,6 @@
 #define PIN_MISO 4
 #define PIN_CS   5
 #define PIN_RST  6
-
-#define SPI_PORT spi0
-#define READ_BIT 0x00
 #define TCP_SOCKET 0
 
 wiz_NetInfo gWIZNETINFO = { .mac = {0x00, 0x08, 0xdc, 0x78, 0x91, 0x71},
@@ -54,8 +51,6 @@ wiz_NetInfo gWIZNETINFO = { .mac = {0x00, 0x08, 0xdc, 0x78, 0x91, 0x71},
 #define ETH_MAX_BUF 2048
 
 unsigned char ethBuf0[ETH_MAX_BUF];
-void print_network_information(void);
-
 
 static inline void cs_select() {
     asm volatile("nop \n nop \n nop");
@@ -75,65 +70,16 @@ static void wiznet_reset() {
     sleep_ms(100);
     gpio_put(PIN_RST, 1);
     sleep_ms(100);
-    bi_decl(bi_1pin_with_name(PIN_RST, "W5500 RST"));
 }
 
 uint8_t  wiznet_read(void) {
     uint8_t rx = 0, tx = 0xFF;
-    spi_read_blocking(SPI_PORT, tx, &rx, 1);
-    return rx;
-        
+    spi_read_blocking(spi_default, tx, &rx, 1);
+    return rx;      
 }
 
 static void wiznet_write(uint8_t reg) {
-    spi_write_blocking(SPI_PORT, &reg, 1);
-}
-    
-
-int main() {
-    int8_t phy_link = 0;
-    uint8_t id = 0;
-    stdio_init_all();
-
-    printf("Hello, W5500! Reading data via SPI...\n");
-
-    // This example will use SPI0 at 0.5MHz.
-    spi_init(SPI_PORT, 5000 * 1000);
-    gpio_set_function(PIN_MISO, GPIO_FUNC_SPI);
-    gpio_set_function(PIN_SCK, GPIO_FUNC_SPI);
-    gpio_set_function(PIN_MOSI, GPIO_FUNC_SPI);
-    // Make the SPI pins available to picotool
-    bi_decl(bi_3pins_with_func(PIN_MISO, PIN_MOSI, PIN_SCK, GPIO_FUNC_SPI));
-
-    // Chip select is active-low, therefore initialise it to a driven-high state
-    gpio_init(PIN_CS);
-    gpio_set_dir(PIN_CS, GPIO_OUT);
-    gpio_put(PIN_CS, 1);
-    // Make the CS pin available to picotool
-    bi_decl(bi_1pin_with_name(PIN_CS, "SPI CS"));
-
-    wiznet_reset();
-
-    reg_wizchip_spi_cbfunc(wiznet_read, wiznet_write);
-    reg_wizchip_cs_cbfunc(cs_select, cs_deselect);
-    id =getVERSIONR();
-    printf("Version is 0x%x\n", id);;
- 
-    do {
-        // Check phy status
-        if(ctlwizchip(CW_GET_PHYLINK, &phy_link) == -1){
-            printf("Unknow PHY LINK status.\r\n");
-            sleep_ms(10);
-        }
-    } while (phy_link == PHY_LINK_OFF);
-    ctlnetwork(CN_SET_NETINFO, &gWIZNETINFO);
-    print_network_information();
-
-    while(1) {
-         loopback_tcps(TCP_SOCKET, ethBuf0, 5000);
-    }
-
-    return 0;
+    spi_write_blocking(spi_default, &reg, 1);
 }
 
 void print_network_information(void) {
@@ -144,3 +90,60 @@ void print_network_information(void) {
 	printf("Gate way   : %d.%d.%d.%d\n\r",gWIZNETINFO.gw[0],gWIZNETINFO.gw[1],gWIZNETINFO.gw[2],gWIZNETINFO.gw[3]);
 	printf("DNS Server : %d.%d.%d.%d\n\r",gWIZNETINFO.dns[0],gWIZNETINFO.dns[1],gWIZNETINFO.dns[2],gWIZNETINFO.dns[3]);
 }
+    
+int main() {
+    int8_t phy_link = 0;
+    uint8_t id = 0;
+    stdio_init_all();
+
+    #if !defined(spi_default) || !defined(PICO_DEFAULT_SPI_SCK_PIN) || !defined(PICO_DEFAULT_SPI_TX_PIN) || !defined(PICO_DEFAULT_SPI_RX_PIN) || !defined(PICO_DEFAULT_SPI_CSN_PIN)
+    #warning ethernet/w5500_spi example requires a board with SPI pins
+    puts("Default SPI pins were not defined");
+    #else
+
+        printf("Hello, W5500! Reading data via SPI...\n");
+
+        // This example will use SPI0 at 0.5MHz.
+        spi_init(spi_default, 5000 * 1000);
+        gpio_set_function(PIN_MISO, GPIO_FUNC_SPI);
+        gpio_set_function(PIN_SCK, GPIO_FUNC_SPI);
+        gpio_set_function(PIN_MOSI, GPIO_FUNC_SPI);
+        // Make the SPI pins available to picotool
+        bi_decl(bi_3pins_with_func(PIN_MISO, PIN_MOSI, PIN_SCK, GPIO_FUNC_SPI));
+        bi_decl(bi_1pin_with_name(PIN_RST, "W5500 RST"));
+
+        // Chip select is active-low, therefore initialise it to a driven-high state
+        gpio_init(PIN_CS);
+        gpio_set_dir(PIN_CS, GPIO_OUT);
+        gpio_put(PIN_CS, 1);
+        // Make the CS pin available to picotool
+        bi_decl(bi_1pin_with_name(PIN_CS, "SPI CS"));
+
+        wiznet_reset();
+
+        // Registers read, write, select and deselect functions 
+        reg_wizchip_spi_cbfunc(wiznet_read, wiznet_write);
+        reg_wizchip_cs_cbfunc(cs_select, cs_deselect);
+        id =getVERSIONR();
+        printf("Version is 0x%x\n", id);;
+    
+        do {
+            // Check phy status
+            if(ctlwizchip(CW_GET_PHYLINK, &phy_link) == -1){
+                printf("Unknown PHY LINK status.\r\n");
+                sleep_ms(10);
+            }
+        // 
+        } while (phy_link == PHY_LINK_OFF);
+        // Sends control signals to set WIZNET info
+        ctlnetwork(CN_SET_NETINFO, &gWIZNETINFO);
+        print_network_information();
+
+        while(1) {
+            loopback_tcps(TCP_SOCKET, ethBuf0, 5000);
+        }
+
+        return 0;
+    #endif
+}
+
