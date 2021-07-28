8523457425e0989fc2bc01012fa7040f81b6a9db
Aayushi Jhaveri <aayushi.jhaveri@raspberrypi.com>
2021-07-28 10:33:45 +0100

    Change comments

--- ethernet/w5500_spi/w5500_spi.c
+++ ethernet/w5500_spi/w5500_spi.c
@@ -14,34 +14,25 @@
 #include "loopback.h"
 
 
-/* Example code to talk to a wiznet MEMS accelerometer and gyroscope.
-   Ignores the magnetometer, that is left as a exercise for the reader.
+/* Example code to talk to send and recieve data from the W5500 chip.
 
-   This is taking to simple approach of simply reading registers. It's perfectly
-   possible to link up an interrupt line and set things up to read from the
-   inbuilt FIFO to make it more useful.
-
-   NOTE: Ensure the device is capable of being driven at 3.3v NOT 5v. The Pico
+   NOTE: Ensure the device is capable of being driven at 3.3V NOT 5V. The Pico
    GPIO (and therefor SPI) cannot be used at 5v.
-
-   You will need to use a level shifter on the I2C lines if you want to run the
-   board at 5v.
 
    Connections on Raspberry Pi Pico board and a generic wiznet board, other
    boards may vary.
-   GPIO 2 (pin 4) SCK/spi0_sclk -> CLK on wiznet board
-   GPIO 3 (pin 5) MOSI/spi0_tx -> MOSIon wiznet board
-   GPIO 4 (pin 6) MISO/spi0_rx-> MISO on wiznet board
-   GPIO 5 (pin 7) Chip select -> NCS on wiznet board
-   GPIO 6 (pin 9) Reset/GPIO -> Reset on wiznet board
-   3.3v (pin 36) -> VCC on wiznet board
-   GND (pin 38)  -> GND on wiznet board
+   GPIO 2 (physical pin 4) SCK/spi0_sclk -> CLK on wiznet board
+   GPIO 3 (physical pin 5) MOSI/spi0_tx -> MOSIon wiznet board
+   GPIO 4 (physical pin 6) MISO/spi0_rx-> MISO on wiznet board
+   GPIO 5 (physical pin 7) Chip select -> NCS on wiznet board
+   GPIO 6 (physical pin 9) Reset/GPIO -> Reset on wiznet board
+   3.3v (physical pin 36) -> VCC on wiznet board
+   GND (physical pin 38)  -> GND on wiznet board
 
    Note: SPI devices can have a number of different naming schemes for pins. See
    the Wikipedia page at https://en.wikipedia.org/wiki/Serial_Peripheral_Interface
    for variations.
-   The particular device used here uses the same pins for I2C and SPI, hence the
-   using of I2C names
+
 */
 
 #define PIN_SCK  2
@@ -54,11 +45,11 @@
 #define READ_BIT 0x00
 #define TCP_SOCKET 0
 
-wiz_NetInfo gWIZNETINFO = { .mac = {0x00,0x08,0xdc,0x78,0x91,0x71},
-                            .ip = {192,168,0,15},
-                            .sn = {255,255,255,255},
-                            .gw = {192,168,0,1},
-                            .dns = {168,126,63,1},
+wiz_NetInfo gWIZNETINFO = { .mac = {0x00, 0x08, 0xdc, 0x78, 0x91, 0x71},
+                            .ip = {192, 168, 0, 15},
+                            .sn = {255, 255, 255, 255},
+                            .gw = {192, 168, 0, 1},
+                            .dns = {168, 126, 63, 1},
                             .dhcp = NETINFO_STATIC};
 #define ETH_MAX_BUF 2048
 
@@ -79,9 +70,6 @@ static inline void cs_deselect() {
 }
 
 static void wiznet_reset() {
-    // Two byte reset. First byte register, second byte data
-    // There are a load more options to set up the device in different ways that could be added here
-
     gpio_set_dir(PIN_RST, GPIO_OUT);
     gpio_put(PIN_RST, 0);
     sleep_ms(100);
@@ -91,27 +79,23 @@ static void wiznet_reset() {
 }
 
 uint8_t  wiznet_read(void) {
-    // For this particular device, we send the device the register we want to read
-    // first, then subsequently read from the device. The register is auto incrementing
-    // so we don't need to keep sending the register we want, just the first.
     uint8_t rx = 0, tx = 0xFF;
-    spi_read_blocking(SPI_PORT, tx,  &rx, 1);
+    spi_read_blocking(SPI_PORT, tx, &rx, 1);
     return rx;
         
 }
 
 static void wiznet_write(uint8_t reg) {
- 
     spi_write_blocking(SPI_PORT, &reg, 1);
 }
     
 
 int main() {
-    int8_t phy_link =0;
-    uint8_t id=0;
+    int8_t phy_link = 0;
+    uint8_t id = 0;
     stdio_init_all();
 
-    printf("Hello, WIZ850! Reading raw data from registers via SPI...\n");
+    printf("Hello, W5500! Reading data via SPI...\n");
 
     // This example will use SPI0 at 0.5MHz.
     spi_init(SPI_PORT, 5000 * 1000);
@@ -121,7 +105,7 @@ int main() {
     // Make the SPI pins available to picotool
     bi_decl(bi_3pins_with_func(PIN_MISO, PIN_MOSI, PIN_SCK, GPIO_FUNC_SPI));
 
-    // Chip select is active-low, so we'll initialise it to a driven-high state
+    // Chip select is active-low, therefore initialise it to a driven-high state
     gpio_init(PIN_CS);
     gpio_set_dir(PIN_CS, GPIO_OUT);
     gpio_put(PIN_CS, 1);
@@ -130,22 +114,23 @@ int main() {
 
     wiznet_reset();
 
-    reg_wizchip_spi_cbfunc(wiznet_read,wiznet_write);
-    reg_wizchip_cs_cbfunc(cs_select,cs_deselect);
+    reg_wizchip_spi_cbfunc(wiznet_read, wiznet_write);
+    reg_wizchip_cs_cbfunc(cs_select, cs_deselect);
     id =getVERSIONR();
     printf("Version is 0x%x\n", id);;
  
-    do{//check phy status
-        if(ctlwizchip(CW_GET_PHYLINK, &phy_link)== -1){
+    do {
+        // Check phy status
+        if(ctlwizchip(CW_GET_PHYLINK, &phy_link) == -1){
             printf("Unknow PHY LINK status.\r\n");
             sleep_ms(10);
         }
-    }while(phy_link == PHY_LINK_OFF);
-    ctlnetwork(CN_SET_NETINFO,&gWIZNETINFO);
+    } while (phy_link == PHY_LINK_OFF);
+    ctlnetwork(CN_SET_NETINFO, &gWIZNETINFO);
     print_network_information();
 
-    while(1){
-         loopback_tcps(TCP_SOCKET, ethBuf0,5000);
+    while(1) {
+         loopback_tcps(TCP_SOCKET, ethBuf0, 5000);
     }
 
     return 0;
