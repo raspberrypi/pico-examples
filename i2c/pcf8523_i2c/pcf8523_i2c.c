9efc9dc58878685d6039fe52e140f7e3b0d9a435
Aayushi Jhaveri <aayushi.jhaveri@raspberrypi.com>
2021-08-05 13:54:16 +0100

    Add alarm feature

--- i2c/pcf8523_i2c/pcf8523_i2c.c
+++ i2c/pcf8523_i2c/pcf8523_i2c.c
@@ -43,44 +43,25 @@ static void pcf8520_reset() {
 
 static void pcf820_write_current_time() {
     // buf[0] is the register to write to
-    // buf[1] is the value that will be written tot he register
+    // buf[1] is the value that will be written to the register
     uint8_t buf[2];
-    
-    // Write the current value to second register
-    buf[0] = 0x03;
-    buf[1] = 0x00;
-    i2c_write_blocking(i2c_default, addr, buf, 2, false);
 
-    // Write the current value to minute register
-    buf[0] = 0x04;
-    buf[1] = 0x00;
-    i2c_write_blocking(i2c_default, addr, buf, 2, false);
+    //Write values for the current time in the array
+    //index 0 -> seconds: bits 4-6 are responsible for the ten's digit and bits 0-3 for the unit's digit
+    //index 1 -> minutes: bits 4-6 are responsible for the ten's digit and bits 0-3 for the unit's digit
+    //index 2 -> hours: bits 4-5 are responsible for the ten's digit and bits 0-3 for the unit's digit
+    //index 3 -> day of the month: bits 4-6 are responsible for the ten's digit and bits 0-3 for the unit's digit
+    //index 4 -> day of the week: where Sunday = 0x00, Monday = 0x01, Tuesday... ...Saturday = 0x06
+    //index 5 -> day of the week: bit 4 is responsible for the ten's digit and bits 0-3 for the unit's digit
+    //index 6 -> years: bits 4-7 are responsible for the ten's digit and bits 0-3 for the unit's digit
 
-    // Write the current value to hour register
-    buf[0] = 0x05;
-    buf[1] = 0x00;
-    i2c_write_blocking(i2c_default, addr, buf, 2, false);
+    uint8_t current_val[7] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
 
-    // Write the current value to day of the month register
-    buf[0] = 0x06;
-    buf[1] = 0x00;
-    i2c_write_blocking(i2c_default, addr, buf, 2, false);
-
-    // Write the current value to the day of the week register
-    // where Sunday = 0x00, Monday = 0x01, Tuesday... ...Saturday = 0x06
-    buf[0] = 0x07;
-    buf[1] = 0x00;
-    i2c_write_blocking(i2c_default, addr, buf, 2, false);
-
-    // Write the current value to the month register
-    buf[0] = 0x08;
-    buf[1] = 0x00;
-    i2c_write_blocking(i2c_default, addr, buf, 2, false);
-
-    // Write the current value to day of the day of the year register
-    buf[0] = 0x09;
-    buf[1] = 0x00;
-    i2c_write_blocking(i2c_default, addr, buf, 2, false);
+    for (int i = 3; i < 10; ++i){
+        buf[0] = i;
+        buf[1] = current_val[i - 3];
+        i2c_write_blocking(i2c_default, addr, buf, 2, false);
+    }
 }
  
 
@@ -96,15 +77,52 @@ static void pcf8520_read_raw(uint8_t *buffer) {
 }
 //#endif
 
-void convert_time(int conv_time[7],uint8_t raw_time[7]){
+void set_alarm(){
+    // buf[0] is the register to write to
+    // buf[1] is the value that will be written to the register
+    uint8_t buf[2];
 
-    conv_time[0]= (10*(int)((raw_time[0] & 0x70)>>4)) + ((int)(raw_time[0] & 0x0F));
-    conv_time[1]= (10*(int)((raw_time[1] & 0x70)>>4)) + ((int)(raw_time[1] & 0x0F));
-    conv_time[2]= (10*(int)((raw_time[2] & 0x30)>>4)) + ((int)(raw_time[2] & 0x0F));
-    conv_time[3]= (10*(int)((raw_time[3] & 0x30)>>4)) + ((int)(raw_time[3] & 0x0F));
+    // Default value of alarm register is 0x80
+    // Set bit 8 of values to 0 to activate that particular alarm
+    // Index 0 -> minutes: bits 4-5 are responsible for the ten's digit and bits 0-3 for the unit's digit
+    // Index 1 -> hours: bits 4-6 are responsible for the ten's digit and bits 0-3 for the unit's digit
+    // Index 2 -> day of the month: bits 4-6 are responsible for the ten's digit and bits 0-3 for the unit's digit
+    // Index 3 -> day of the week: where Sunday = 0x00, Monday = 0x01, Tuesday... ...Saturday = 0x06
+
+    uint8_t alarm_val[4] = {0x01, 0x80, 0x80, 0x80};
+    // Writes alarm values to registers
+    for (int i = 10; i < 14; ++i){
+        buf[0] = (uint8_t) i;
+        buf[1] = alarm_val[i - 10];
+        i2c_write_blocking(i2c_default, addr, buf, 2, false);
+    }
+ 
+}
+
+void check_alarm(){
+    // Checks bit 3 of control register 2 for alarm flags
+    uint8_t status[1];
+    uint8_t val = 0x01;
+    i2c_write_blocking(i2c_default, addr, &val, 1, true); // true to keep master control of bus
+    i2c_read_blocking(i2c_default, addr, status, 1, false);
+
+    if ((status[0] & 0x08)==0x08){
+        printf("ALARM RINGING");
+    } else {
+        printf("Alarm not triggered yet");
+    }
+
+}
+
+void convert_time(int conv_time[7],uint8_t raw_time[7]){
+    // Converts raw data into time
+    conv_time[0]= (10*(int)((raw_time[0] & 0x70) >> 4)) + ((int)(raw_time[0] & 0x0F));
+    conv_time[1]= (10*(int)((raw_time[1] & 0x70) >> 4)) + ((int)(raw_time[1] & 0x0F));
+    conv_time[2]= (10*(int)((raw_time[2] & 0x30) >> 4)) + ((int)(raw_time[2] & 0x0F));
+    conv_time[3]= (10*(int)((raw_time[3] & 0x30) >> 4)) + ((int)(raw_time[3] & 0x0F));
     conv_time[4]= (int)(raw_time[4] & 0x07);
-    conv_time[5]= (int)(raw_time[5] & 0x1F);
-    conv_time[6]= (int)raw_time[6];
+    conv_time[5]= (10*(int)((raw_time[5] & 0x10) >> 4)) + ((int)(raw_time[5] & 0x0F));
+    conv_time[6]= (10*(int)((raw_time[6] & 0xF0) >> 4)) + ((int)(raw_time[6] & 0x0F));
 }
 
 int main() {
@@ -127,6 +145,8 @@ int main() {
     pcf8520_reset();
 
     pcf820_write_current_time();
+    set_alarm();
+    check_alarm();
 
     uint8_t raw_time[7];
     int real_time[7];
@@ -136,11 +156,13 @@ int main() {
         
         pcf8520_read_raw(raw_time);
         convert_time(real_time, raw_time);
+
         printf("Time: %02d : %02d : %02d\n",real_time[2],real_time[1],real_time[0]);
         printf("Date: %s %02d / %02d / %02d\n",days_of_week[real_time[4]], real_time[3], real_time[5], real_time[6]);
+        check_alarm();
         
         
-        sleep_ms(1000);
+        sleep_ms(500);
 
         // Clear terminal 
         printf("\e[1;1H\e[2J");
