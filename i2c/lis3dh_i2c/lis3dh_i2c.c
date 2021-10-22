ab490a39307b201268796cdfca2621073b9a592b
graham sanderson <graham.sanderson@raspberrypi.com>
2021-10-22 15:17:44 -0500

    Additional examples for specific h/w by our interns
    adc/microphone_adc - Read analog values from a microphone and plot the measured sound amplitude.
    i2c/bmp280_i2c - Read and convert temperature and pressure data from a BMP280 sensor, attached to an I2C bus.
    i2c/lis3dh_i2c - Read acceleration and temperature value from a LIS3DH sensor via I2C
    i2c/mcp9808_i2c - Read temperature, set limits and raise alerts when limits are surpassed.
    i2c/mma8451_i2c - Read acceleration from a MMA8451 accelerometer and set range and precision for the data.
    i2c/mpl3115a2_i2c - Interface with an MPL3115A2 altimeter, exploring interrupts and advanced board features, via I2C.
    i2c/oled_i2c - Convert and display a bitmap on a 128x32 SSD1306-driven OLED display
    i2c/pa1010d_i2c - Read GPS location data, parse and display data via I2C.
    i2c/pcf8523_i2c - Read time and date values from a real time clock. Set current time and alarms on it.
    uart/lcd_uart - Display text and symbols on a 16x02 RGB LCD display via UART

---
+++ i2c/lis3dh_i2c/lis3dh_i2c.c
@@ -0,0 +1,129 @@
+/**
+ * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include "pico/stdlib.h"
+#include "pico/binary_info.h"
+#include "hardware/i2c.h"
+
+/* Example code to talk to a LIS3DH Mini GPS module.
+
+   This example reads data from all 3 axes of the accelerometer and uses an auxillary ADC to output temperature values.
+
+   Connections on Raspberry Pi Pico board, other boards may vary.
+
+   GPIO PICO_DEFAULT_I2C_SDA_PIN (On Pico this is 4 (physical pin 6)) -> SDA on LIS3DH board
+   GPIO PICO_DEFAULT_I2C_SCK_PIN (On Pico this is 5 (physical pin 7)) -> SCL on LIS3DH board
+   3.3v (physical pin 36) -> VIN on LIS3DH board
+   GND (physical pin 38)  -> GND on LIS3DH board
+*/
+
+// By default this device is on bus address 0x18
+
+const int ADDRESS = 0x18;
+const uint8_t CTRL_REG_1 = 0x20;
+const uint8_t CTRL_REG_4 = 0x23;
+const uint8_t TEMP_CFG_REG = 0xC0;
+
+#ifdef i2c_default
+
+void lis3dh_init() {
+    uint8_t buf[2];
+
+    // Turn normal mode and 1.344kHz data rate on
+    buf[0] = CTRL_REG_1;
+    buf[1] = 0x97;
+    i2c_write_blocking(i2c_default, ADDRESS, buf, 2, false);
+
+    // Turn block data update on (for temperature sensing)
+    buf[0] = CTRL_REG_4;
+    buf[1] = 0x80;
+    i2c_write_blocking(i2c_default, ADDRESS, buf, 2, false);
+
+    // Turn auxillary ADC on
+    buf[0] = TEMP_CFG_REG;
+    buf[1] = 0xC0;
+    i2c_write_blocking(i2c_default, ADDRESS, buf, 2, false);
+}
+
+void lis3dh_calc_value(uint16_t raw_value, float *final_value, bool isAccel) {
+    // Convert with respect to the value being temperature or acceleration reading 
+    float scaling;
+    float senstivity = 0.004f; // g per unit
+
+    if (isAccel == true) {
+        scaling = 64 / senstivity;
+    } else {
+        scaling = 64;
+    }
+
+    // raw_value is signed
+    *final_value = (float) ((int16_t) raw_value) / scaling;
+}
+
+void lis3dh_read_data(uint8_t reg, float *final_value, bool IsAccel) {
+    // Read two bytes of data and store in a 16 bit data structure
+    uint8_t lsb;
+    uint8_t msb;
+    uint16_t raw_accel;
+    i2c_write_blocking(i2c_default, ADDRESS, &reg, 1, true);
+    i2c_read_blocking(i2c_default, ADDRESS, &lsb, 1, false);
+
+    reg |= 0x01;
+    i2c_write_blocking(i2c_default, ADDRESS, &reg, 1, true);
+    i2c_read_blocking(i2c_default, ADDRESS, &msb, 1, false);
+
+    raw_accel = (msb << 8) | lsb;
+
+    lis3dh_calc_value(raw_accel, final_value, IsAccel);
+}
+
+#endif
+
+int main() {
+    stdio_init_all();
+#if !defined(i2c_default) || !defined(PICO_DEFAULT_I2C_SDA_PIN) || !defined(PICO_DEFAULT_I2C_SCL_PIN)
+#warning i2c/lis3dh_i2c example requires a board with I2C pins
+    puts("Default I2C pins were not defined");
+#else
+    printf("Hello, LIS3DH! Reading raw data from registers...\n");
+
+    // This example will use I2C0 on the default SDA and SCL pins (4, 5 on a Pico)
+    i2c_init(i2c_default, 400 * 1000);
+    gpio_set_function(PICO_DEFAULT_I2C_SDA_PIN, GPIO_FUNC_I2C);
+    gpio_set_function(PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C);
+    gpio_pull_up(PICO_DEFAULT_I2C_SDA_PIN);
+    gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN);
+    // Make the I2C pins available to picotool
+    bi_decl(bi_2pins_with_func(PICO_DEFAULT_I2C_SDA_PIN, PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C));
+
+    float x_accel, y_accel, z_accel, temp;
+
+    lis3dh_init();
+
+    while (1) {
+        lis3dh_read_data(0x28, &x_accel, true);
+        lis3dh_read_data(0x2A, &y_accel, true);
+        lis3dh_read_data(0x2C, &z_accel, true);
+        lis3dh_read_data(0x0C, &temp, false);
+
+        // Display data 
+        printf("TEMPERATURE: %.3f%cC\n", temp, 176);
+        // Acceleration is read as a multiple of g (gravitational acceleration on the Earth's surface)
+        printf("ACCELERATION VALUES: \n");
+        printf("X acceleration: %.3fg\n", x_accel);
+        printf("Y acceleration: %.3fg\n", y_accel);
+        printf("Z acceleration: %.3fg\n", z_accel);
+
+        sleep_ms(500);
+
+        // Clear terminal 
+        printf("\e[1;1H\e[2J");
+    }
+#endif
+    return 0;
+}
---
+++ i2c/pa1010d_i2c/pa1010d_i2c.c
@@ -0,0 +1,156 @@
+/**
+ * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include "pico/stdlib.h"
+#include "pico/binary_info.h"
+#include "hardware/i2c.h"
+#include "string.h"
+
+/* Example code to talk to a PA1010D Mini GPS module.
+
+   This example reads the Recommended Minimum Specific GNSS Sentence, which includes basic location and time data, each second, formats and displays it.
+
+   Connections on Raspberry Pi Pico board, other boards may vary.
+
+   GPIO PICO_DEFAULT_I2C_SDA_PIN (On Pico this is 4 (physical pin 6)) -> SDA on PA1010D board
+   GPIO PICO_DEFAULT_I2C_SCK_PIN (On Pico this is 5 (physical pin 7)) -> SCL on PA1010D board
+   3.3v (physical pin 36) -> VCC on PA1010D board
+   GND (physical pin 38)  -> GND on PA1010D board
+*/
+
+const int addr = 0x10;
+const int max_read = 250;
+
+#ifdef i2c_default
+
+void pa1010d_write_command(const char command[], int com_length) {
+    // Convert character array to bytes for writing
+    uint8_t int_command[com_length];
+
+    for (int i = 0; i < com_length; ++i) {
+        int_command[i] = command[i];
+        i2c_write_blocking(i2c_default, addr, &int_command[i], 1, true);
+    }
+}
+
+void pa1010d_parse_string(char output[], char protocol[]) {
+    // Finds location of protocol message in output
+    char *com_index = strstr(output, protocol);
+    int p = com_index - output;
+
+    // Splits components of output sentence into array
+    int no_of_fields = 14;
+    int max_len = 15;
+
+    int n = 0;
+    int m = 0;
+
+    char gps_data[no_of_fields][max_len];
+    memset(gps_data, 0, sizeof(gps_data));
+
+    bool complete = false;
+    while (output[p] != '$' && n < max_len && complete == false) {
+        if (output[p] == ',' || output[p] == '*') {
+            n += 1;
+            m = 0;
+        } else {
+            gps_data[n][m] = output[p];
+            // Checks if sentence is complete
+            if (m < no_of_fields) {
+                m++;
+            } else {
+                complete = true;
+            }
+        }
+        p++;
+    }
+
+    // Displays GNRMC data
+    // Similarly, additional if statements can be used to add more protocols 
+    if (strcmp(protocol, "GNRMC") == 0) {
+        printf("Protcol:%s\n", gps_data[0]);
+        printf("UTC Time: %s\n", gps_data[1]);
+        printf("Status: %s\n", gps_data[2][0] == 'V' ? "Data invalid. GPS fix not found." : "Data Valid");
+        printf("Latitude: %s\n", gps_data[3]);
+        printf("N/S indicator: %s\n", gps_data[4]);
+        printf("Longitude: %s\n", gps_data[5]);
+        printf("E/W indicator: %s\n", gps_data[6]);
+        printf("Speed over ground: %s\n", gps_data[7]);
+        printf("Course over ground: %s\n", gps_data[8]);
+        printf("Date: %c%c/%c%c/%c%c\n", gps_data[9][0], gps_data[9][1], gps_data[9][2], gps_data[9][3], gps_data[9][4],
+               gps_data[9][5]);
+        printf("Magnetic Variation: %s\n", gps_data[10]);
+        printf("E/W degree indicator: %s\n", gps_data[11]);
+        printf("Mode: %s\n", gps_data[12]);
+        printf("Checksum: %c%c\n", gps_data[13][0], gps_data[13][1]);
+    }
+}
+
+void pa1010d_read_raw(char numcommand[]) {
+    uint8_t buffer[max_read];
+
+    int i = 0;
+    bool complete = false;
+
+    i2c_read_blocking(i2c_default, addr, buffer, max_read, false);
+
+    // Convert bytes to characters
+    while (i < max_read && complete == false) {
+        numcommand[i] = buffer[i];
+        // Stop converting at end of message 
+        if (buffer[i] == 10 && buffer[i + 1] == 10) {
+            complete = true;
+        }
+        i++;
+    }
+}
+
+#endif
+
+int main() {
+    stdio_init_all();
+#if !defined(i2c_default) || !defined(PICO_DEFAULT_I2C_SDA_PIN) || !defined(PICO_DEFAULT_I2C_SCL_PIN)
+#warning i2c/mpu6050_i2c example requires a board with I2C pins
+    puts("Default I2C pins were not defined");
+#else
+
+    char numcommand[max_read];
+
+    // Decide which protocols you would like to retrieve data from
+    char init_command[] = "$PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29\r\n";
+
+    // This example will use I2C0 on the default SDA and SCL pins (4, 5 on a Pico)
+    i2c_init(i2c_default, 400 * 1000);
+    gpio_set_function(PICO_DEFAULT_I2C_SDA_PIN, GPIO_FUNC_I2C);
+    gpio_set_function(PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C);
+    gpio_pull_up(PICO_DEFAULT_I2C_SDA_PIN);
+    gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN);
+
+    // Make the I2C pins available to picotool
+    bi_decl(bi_2pins_with_func(PICO_DEFAULT_I2C_SDA_PIN, PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C));
+
+    printf("Hello, PA1010D! Reading raw data from module...\n");
+
+    pa1010d_write_command(init_command, sizeof(init_command));
+
+    while (1) {
+        // Clear array
+        memset(numcommand, 0, max_read);
+        // Read and re-format
+        pa1010d_read_raw(numcommand);
+        pa1010d_parse_string(numcommand, "GNRMC");
+
+        // Wait for data to refresh
+        sleep_ms(1000);
+
+        // Clear terminal 
+        printf("\e[1;1H\e[2J");
+    }
+#endif
+    return 0;
+}
