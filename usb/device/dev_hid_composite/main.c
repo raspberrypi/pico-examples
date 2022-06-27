3fbc50d2db1c6b4d5baff37bf918950eb05dd54e
Liam Fraser <liam@raspberrypi.com>
2022-06-27 15:44:06 +0100

    Update dev_hid_composite to match latest TinyUSB api

--- usb/device/dev_hid_composite/main.c
+++ usb/device/dev_hid_composite/main.c
@@ -56,7 +56,9 @@ void hid_task(void);
 int main(void)
 {
   board_init();
-  tusb_init();
+
+  // init device stack on configured roothub port
+  tud_init(BOARD_TUD_RHPORT);
 
   while (1)
   {
@@ -223,7 +225,7 @@ void hid_task(void)
 // Invoked when sent REPORT successfully to host
 // Application can use this to send the next report
 // Note: For composite reports, report[0] is report ID
-void tud_hid_report_complete_cb(uint8_t instance, uint8_t const* report, uint8_t len)
+void tud_hid_report_complete_cb(uint8_t instance, uint8_t const* report, uint16_t len)
 {
   (void) instance;
   (void) len;
