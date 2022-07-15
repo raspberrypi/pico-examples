a2f8385a3153df4f24e27029d6196225aad0b328
Liam Fraser <liam@raspberrypi.com>
2022-07-15 10:38:54 +0100

    Fix incorrect gpio / gpout in hello_gpout

--- clocks/hello_gpout/hello_gpout.c
+++ clocks/hello_gpout/hello_gpout.c
@@ -15,9 +15,9 @@ int main() {
 
     // Output clk_sys / 10 to gpio 21, etc...
     clock_gpio_init(21, CLOCKS_CLK_GPOUT0_CTRL_AUXSRC_VALUE_CLK_SYS, 10);
-    clock_gpio_init(23, CLOCKS_CLK_GPOUT0_CTRL_AUXSRC_VALUE_CLK_USB, 10);
-    clock_gpio_init(24, CLOCKS_CLK_GPOUT0_CTRL_AUXSRC_VALUE_CLK_ADC, 10);
-    clock_gpio_init(26, CLOCKS_CLK_GPOUT0_CTRL_AUXSRC_VALUE_CLK_RTC, 10);
+    clock_gpio_init(23, CLOCKS_CLK_GPOUT1_CTRL_AUXSRC_VALUE_CLK_USB, 10);
+    clock_gpio_init(24, CLOCKS_CLK_GPOUT2_CTRL_AUXSRC_VALUE_CLK_ADC, 10);
+    clock_gpio_init(25, CLOCKS_CLK_GPOUT3_CTRL_AUXSRC_VALUE_CLK_RTC, 10);
 
     return 0;
 }
