2529ec79ccc5fdf42c6ec585194d5d6feed8f031
Cyndi Cavanaugh <cynthiacavanaugh1@gmail.com>
2023-01-15 10:19:09 -0600

    optimized

--- cynthia/digital_display/digital_display.c
+++ cynthia/digital_display/digital_display.c
@@ -1,51 +1,66 @@
 #include "pico/stdlib.h"
 
+/*
+ *  LED digital display segments and their corresponding pins
+ */
+const uint SEGMENT_DP = 0;
+const uint SEGMENT_A = 1;
+const uint SEGMENT_B = 2;
+const uint SEGMENT_C = 3;
+const uint SEGMENT_D = 4;
+const uint SEGMENT_E = 5;
+const uint SEGMENT_F = 6;
+const uint SEGMENT_G = 7;
+const uint SEGMENTS[] = { SEGMENT_DP, SEGMENT_A, SEGMENT_B, SEGMENT_C, SEGMENT_D, SEGMENT_E, SEGMENT_F, SEGMENT_G };
+
+/// @brief sets a segment's pin to output
+/// @param segment the segment to set to output
+void set_dir_out(uint segment) {
+    gpio_set_dir(segment, GPIO_OUT);
+}
+
+/// @brief initializes a segment's pin
+/// @param segment the segment to initialize
+void init(uint segment) {
+    gpio_init(segment);
+}
+
+/// @brief set or turn on a segment
+/// @param segment the segment to set
+void set(uint segment) {
+    gpio_put(segment, 1);
+}
+
+/// @brief reset or turn off a segment
+/// @param segment the segment to reset
+void reset(uint segment) {
+    gpio_put(segment, 0);
+}
+
+/// @brief call the specified function on an array of segments
+/// @param segments array of segments on which to call the function
+/// @param segment_count size of the incoming array
+/// @param something pointer to a function which takes a uint (the segment) and does something to it
+void segments_do(uint segments[], size_t segment_count, void (*something)(uint)) {
+    for(int i = 0; i < segment_count; i++) {
+        something(segments[i]);
+    }
+}
+
+/// @brief call the specified function on all segments
+/// @param something pointer to a function which takes a uint (the segment) and does something to it
+void all_segments_do(void (*something)(uint)) {
+    segments_do((uint *)SEGMENTS, sizeof(SEGMENTS)/sizeof(SEGMENTS[0]), something);
+}
+
 int main() {
-    const uint SEGMENT_DP = 0;
-    const uint SEGMENT_A = 1;
-    const uint SEGMENT_B = 2;
-    const uint SEGMENT_C = 3;
-    const uint SEGMENT_D = 4;
-    const uint SEGMENT_E = 5;
-    const uint SEGMENT_F = 6;
-    const uint SEGMENT_G = 7;
-
-    gpio_init(SEGMENT_DP);
-    gpio_init(SEGMENT_A);
-    gpio_init(SEGMENT_B);
-    gpio_init(SEGMENT_C);
-    gpio_init(SEGMENT_D);
-    gpio_init(SEGMENT_E);
-    gpio_init(SEGMENT_F);
-    gpio_init(SEGMENT_G);
-
-    gpio_set_dir(SEGMENT_DP, GPIO_OUT);
-    gpio_set_dir(SEGMENT_A, GPIO_OUT);
-    gpio_set_dir(SEGMENT_B, GPIO_OUT);
-    gpio_set_dir(SEGMENT_C, GPIO_OUT);
-    gpio_set_dir(SEGMENT_D, GPIO_OUT);
-    gpio_set_dir(SEGMENT_E, GPIO_OUT);
-    gpio_set_dir(SEGMENT_F, GPIO_OUT);
-    gpio_set_dir(SEGMENT_G, GPIO_OUT);
+    all_segments_do(init);
+    all_segments_do(set_dir_out);
 
     while (true) {
-        gpio_put(SEGMENT_DP, 1);
-        gpio_put(SEGMENT_A, 1);
-        gpio_put(SEGMENT_B, 1);
-        gpio_put(SEGMENT_C, 1);
-        gpio_put(SEGMENT_D, 1);
-        gpio_put(SEGMENT_E, 1);
-        gpio_put(SEGMENT_F, 1);
-        gpio_put(SEGMENT_G, 1);
+        all_segments_do(set);
         sleep_ms(250);
-        gpio_put(SEGMENT_DP, 0);
-        gpio_put(SEGMENT_A, 0);
-        gpio_put(SEGMENT_B, 0);
-        gpio_put(SEGMENT_C, 0);
-        gpio_put(SEGMENT_D, 0);
-        gpio_put(SEGMENT_E, 0);
-        gpio_put(SEGMENT_F, 0);
-        gpio_put(SEGMENT_G, 0);
+        all_segments_do(reset);
         sleep_ms(250);
     }
 }
