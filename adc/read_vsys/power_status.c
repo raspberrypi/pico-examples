d6a8d2981be1773a048ee5b1e361b5af85071209
Peter Harper <peter.harper@raspberrypi.com>
2023-06-08 12:23:02 +0100

    In read_vsys ignore initial readings
    We were doing this for pico_w but it seems it's needed for pico as well.

--- adc/read_vsys/power_status.c
+++ adc/read_vsys/power_status.c
@@ -49,13 +49,11 @@ int power_voltage(float *voltage_result) {
     adc_fifo_setup(true, false, 0, false, false);
     adc_run(true);
 
-#if CYW43_USES_VSYS_PIN
-    // We seem to read low values from cyw43 sometimes - this seems to fix it
+    // We seem to read low values initially - this seems to fix it
     int ignore_count = PICO_POWER_SAMPLE_COUNT;
     while (!adc_fifo_is_empty() || ignore_count-- > 0) {
         (void)adc_fifo_get_blocking();
     }
-#endif
 
     // read vsys
     uint32_t vsys = 0;
