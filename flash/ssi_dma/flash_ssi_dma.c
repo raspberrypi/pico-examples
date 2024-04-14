c759dd2eed0f0523f4635a616b8ba664c1e7748a
FranCDoc <fchiesadoc@gmail.com>
2024-04-14 02:10:28 -0300

    adding sleep to give time to connect to minicom

--- flash/ssi_dma/flash_ssi_dma.c
+++ flash/ssi_dma/flash_ssi_dma.c
@@ -67,6 +67,8 @@ uint32_t *expect = (uint32_t *) XIP_NOCACHE_NOALLOC_BASE;
 
 int main() {
     stdio_init_all();
+    
+    sleep_ms(2500);
     memset(rxdata, 0, DATA_SIZE_WORDS * sizeof(uint32_t));
 
     printf("Starting DMA\n");
