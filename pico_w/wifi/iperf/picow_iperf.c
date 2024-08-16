e927d07a30488ef4e82c40695f586b6b4c2ca779
Peter Harper <peter.harper@raspberrypi.com>
2024-08-16 14:31:58 +0100

    Fix compile issues with LWIP_SINGLE_NETIF
    Fixes #514

--- pico_w/wifi/iperf/picow_iperf.c
+++ pico_w/wifi/iperf/picow_iperf.c
@@ -72,7 +72,7 @@ int main() {
     ip4_addr_set_u32(&clientaddr, ipaddr_addr(xstr(IPERF_SERVER_IP)));
     assert(lwiperf_start_tcp_client_default(&clientaddr, &iperf_report, NULL) != NULL);
 #else
-    printf("\nReady, running iperf server at %s\n", ip4addr_ntoa(netif_ip4_addr(netif_list)));
+    printf("\nReady, running iperf server at %s\n", ip4addr_ntoa(netif_ip4_addr(netif_default)));
     lwiperf_start_tcp_server_default(&iperf_report, NULL);
 #endif
     cyw43_arch_lwip_end();
