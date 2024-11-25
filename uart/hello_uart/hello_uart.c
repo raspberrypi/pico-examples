42ad1bca16982329a1a4a7b9ccc4acf29694e57d
abdalla mohamed <abdalla1917mohamed@gmail.com>
2024-11-25 11:58:02 +0200

    Update hello_uart.c
    Function_select doesn't exist anymore in pico-sdk libraries and doesn't compile , so the lines : 
    
     - gpio_set_function(UART_TX_PIN, UART_FUNCSEL_NUM(UART_ID, UART_TX_PIN));
     - gpio_set_function(UART_RX_PIN, UART_FUNCSEL_NUM(UART_ID, UART_RX_PIN));
    
    are replaced by : 
    gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
    gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);

--- uart/hello_uart/hello_uart.c
+++ uart/hello_uart/hello_uart.c
@@ -23,10 +23,9 @@ int main() {
     // Set up our UART with the required speed.
     uart_init(UART_ID, BAUD_RATE);
 
-    // Set the TX and RX pins by using the function select on the GPIO
-    // Set datasheet for more information on function select
-    gpio_set_function(UART_TX_PIN, UART_FUNCSEL_NUM(UART_ID, UART_TX_PIN));
-    gpio_set_function(UART_RX_PIN, UART_FUNCSEL_NUM(UART_ID, UART_RX_PIN));
+    // Set the TX and RX pins using enum GPIO_FUNC_UART 
+    gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
+    gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
 
     // Use some the various UART functions to send out data
     // In a default system, printf will also output via the default UART
