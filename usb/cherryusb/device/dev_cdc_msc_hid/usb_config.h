c75e374e07d3349d862d37331b1407ed4a223b28
sakumisu <1203593632@qq.com>
2025-01-25 17:28:37 +0800

    feat(cherryusb): add cherryusb device & host demo
    Signed-off-by: sakumisu <1203593632@qq.com>

---
+++ usb/cherryusb/device/dev_cdc_msc_hid/usb_config.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2022, sakumisu
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef CHERRYUSB_CONFIG_H
+#define CHERRYUSB_CONFIG_H
+
+#include "usb_config_examples_common.h"
+
+#endif
