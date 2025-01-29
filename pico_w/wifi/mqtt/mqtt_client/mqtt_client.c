e239bc4d23ad91447b5429431499bd4d64197e3f
Peter Harper <peter.harper@raspberrypi.com>
2025-01-29 17:06:56 +0000

    Tidy up mqtt example
    Fix review comments and build warnings.
    Use MQTT_SERVER build variable instead of hard coding IP address.
    Make a DNS request for the server.
    Use async timer for publishing the temperature.

--- pico_w/wifi/mqtt/mqtt_client/mqtt_client.c
+++ pico_w/wifi/mqtt/mqtt_client/mqtt_client.c
@@ -1,43 +1,48 @@
+/**
+ * Copyright (c) 2022 Raspberry Pi (Trading) Ltd.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
 //
 // Created by elliot on 25/05/24.
 //
 #include "pico/stdlib.h"
+#include "pico/cyw43_arch.h"
+#include "pico/unique_id.h"
 #include "hardware/gpio.h"
 #include "hardware/irq.h"
 #include "hardware/adc.h"
-#include "pico/cyw43_arch.h"
 #include "lwip/apps/mqtt.h"
-
+#include "lwip/dns.h"
 
 // Temperature
-#define TEMPERATURE_UNITS 'C'
+#ifndef TEMPERATURE_UNITS
+#define TEMPERATURE_UNITS 'C' // Set to 'F' for Fahrenheit
+#endif
 
-// WiFi
-#define WIFI_SSID "YOUR_WIFI_SSID"
-#define WIFI_PASSWORD "YOUR_WIFI_PASSWD"
-// MQTT
-#define PORT  1883
-#define MQTT_CLIENT_ID "clientID"
-#define MQTT_BROKER_IP "YOUR_BROKER_IP"
+#ifndef MQTT_SERVER
+#error Need to define MQTT_SERVER
+#endif
 
-
+// MQTT parameters
+#ifndef MQTT_PORT
+#define MQTT_PORT 1883
+#endif
 
 typedef struct {
     mqtt_client_t* mqtt_client_inst;
     struct mqtt_connect_client_info_t mqtt_client_info;
-    uint8_t data[MQTT_OUTPUT_RINGBUF_SIZE];
-    uint8_t topic[100];
+    char data[MQTT_OUTPUT_RINGBUF_SIZE];
+    char topic[100];
     uint32_t len;
+    ip_addr_t mqtt_server_address;
 } MQTT_CLIENT_DATA_T;
-
-MQTT_CLIENT_DATA_T *mqtt;
-
-
 
 /* References for this implementation:
  * raspberry-pi-pico-c-sdk.pdf, Section '4.1.1. hardware_adc'
  * pico-examples/adc/adc_console/adc_console.c */
-float read_onboard_temperature(const char unit) {
+static float read_onboard_temperature(const char unit) {
 
     /* 12-bit conversion, assume max value == ADC_VREF == 3.3 V */
     const float conversionFactor = 3.3f / (1 << 12);
@@ -45,7 +50,7 @@ float read_onboard_temperature(const char unit) {
     float adc = (float)adc_read() * conversionFactor;
     float tempC = 27.0f - (adc - 0.706f) / 0.001721f;
 
-    if (unit == 'C') {
+    if (unit == 'C' || unit != 'F') {
         return tempC;
     } else if (unit == 'F') {
         return tempC * 9 / 5 + 32;
@@ -54,8 +59,8 @@ float read_onboard_temperature(const char unit) {
     return -1.0f;
 }
 
-void control_led(bool on) {
-    // Public state on /state topic and on/off led board
+static void control_led(MQTT_CLIENT_DATA_T *state, bool on) {
+    // Publish state on /state topic and on/off led board
     const char* message = on ? "On" : "Off";
     if (on)
         cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
@@ -63,98 +68,134 @@ void control_led(bool on) {
         cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
 
     char state_topic[128];
-    snprintf(state_topic, sizeof(state_topic), "%s/state", mqtt->topic);
-    mqtt_publish(mqtt->mqtt_client_inst, state_topic, message, strlen(message), 0, 0, NULL, NULL);
+    snprintf(state_topic, sizeof(state_topic), "%s/state", state->topic);
+    mqtt_publish(state->mqtt_client_inst, state_topic, message, strlen(message), 0, 0, NULL, NULL);
 }
 
-void publish_temperature() {
-    //Public temperature on /temperature topic
+static void publish_temperature(MQTT_CLIENT_DATA_T *state) {
+    static float old_temperature;
+    const char temperature_key[] = "/temperature";
     float temperature = read_onboard_temperature(TEMPERATURE_UNITS);
-    char temp_str[16];
-    snprintf(temp_str, sizeof(temp_str), "%.2f", temperature);
-    mqtt_publish(mqtt->mqtt_client_inst, "/temperature", temp_str, strlen(temp_str), 0, 0, NULL, NULL);
+    if (temperature != old_temperature) {
+        old_temperature = temperature;
+        // Publish temperature on /temperature topic
+        char temp_str[16];
+        snprintf(temp_str, sizeof(temp_str), "%.2f", temperature);
+        printf("Publishing %s to %s\n", temp_str, temperature_key);
+        mqtt_publish(state->mqtt_client_inst, temperature_key, temp_str, strlen(temp_str), 0, 0, NULL, NULL);
+    }
 }
+
 static void mqtt_incoming_data_cb(void *arg, const u8_t *data, u16_t len, u8_t flags) {
-    MQTT_CLIENT_DATA_T* mqtt_client = (MQTT_CLIENT_DATA_T*)arg;
-    strncpy(mqtt_client->data, data, len);
-    mqtt_client->len = len;
-    mqtt_client->data[len] = '\0';
+    MQTT_CLIENT_DATA_T* state = (MQTT_CLIENT_DATA_T*)arg;
+    strncpy(state->data, (const char *)data, len);
+    state->len = len;
+    state->data[len] = '\0';
 
-    printf("Topic: %s, Message: %s\n", mqtt_client->topic, mqtt_client->data);
+    printf("Topic: %s, Message: %s\n", state->topic, state->data);
 
-    if (strcmp(mqtt->topic, "/led") == 0)
+    if (strcmp(state->topic, "/led") == 0)
     {
-        if (strcmp((const char *)mqtt_client->data, "On") == 0)
-            control_led(true);
-        else if  (strcmp((const char *)mqtt_client->data, "Off") == 0)
-            control_led(false);
+        if (lwip_stricmp((const char *)state->data, "On") == 0)
+            control_led(state, true);
+        else if  (lwip_stricmp((const char *)state->data, "Off") == 0)
+            control_led(state, false);
     }
 }
 
 static void mqtt_incoming_publish_cb(void *arg, const char *topic, u32_t tot_len) {
     MQTT_CLIENT_DATA_T* mqtt_client = (MQTT_CLIENT_DATA_T*)arg;
-    strcpy(mqtt_client->topic, topic);
+    strncpy(mqtt_client->topic, topic, sizeof(mqtt_client->topic));
 }
 
+static void temperature_worker_fn(async_context_t *context, async_at_time_worker_t *worker) {
+    MQTT_CLIENT_DATA_T* state = (MQTT_CLIENT_DATA_T*)worker->user_data;
+    publish_temperature(state);
+    async_context_add_at_time_worker_in_ms(context, worker, 10000); // repeat in 10s
+}
+static async_at_time_worker_t temperature_worker = { .do_work = temperature_worker_fn };
+
 static void mqtt_connection_cb(mqtt_client_t *client, void *arg, mqtt_connection_status_t status) {
-    MQTT_CLIENT_DATA_T* mqtt_client = (MQTT_CLIENT_DATA_T*)arg;
     if (status == MQTT_CONNECT_ACCEPTED) {
         mqtt_sub_unsub(client, "/led", 0, NULL, arg, 1);
         printf("Connected to the /led topic successfully\n");
+
+        // Publish temperature every 10 sec if it's changed
+        temperature_worker.user_data = arg;
+        async_context_add_at_time_worker_in_ms(cyw43_arch_async_context(), &temperature_worker, 0);
+    } else {
+        panic("Unexpected status");
     }
 }
-int main() {
 
+static void start_client(MQTT_CLIENT_DATA_T *state) {
+    state->mqtt_client_inst = mqtt_client_new();
+    if (!state->mqtt_client_inst) {
+        panic("MQTT client instance creation error");
+    }
+    printf("IP address of this device %s\n", ipaddr_ntoa(&(netif_list->ip_addr)));
+    printf("Connecting to mqtt server at %s\n", ipaddr_ntoa(&state->mqtt_server_address));
+
+    cyw43_arch_lwip_begin();
+    if (mqtt_client_connect(state->mqtt_client_inst, &state->mqtt_server_address, MQTT_PORT, mqtt_connection_cb, state, &state->mqtt_client_info) != ERR_OK) {
+        panic("MQTT broker connection error");
+    }
+    mqtt_set_inpub_callback(state->mqtt_client_inst, mqtt_incoming_publish_cb, mqtt_incoming_data_cb, state);
+    cyw43_arch_lwip_end();
+}
+
+// Call back with a DNS result
+static void dns_found(const char *hostname, const ip_addr_t *ipaddr, void *arg) {
+    MQTT_CLIENT_DATA_T *state = (MQTT_CLIENT_DATA_T*)arg;
+    if (ipaddr) {
+        state->mqtt_server_address = *ipaddr;
+        start_client(state);
+    } else {
+        panic("dns request failed");
+    }
+}
+
+int main(void) {
     stdio_init_all();
 
     adc_init();
     adc_set_temp_sensor_enabled(true);
     adc_select_input(4);
 
-    mqtt = (MQTT_CLIENT_DATA_T*)calloc(1, sizeof(MQTT_CLIENT_DATA_T));
-    if (!mqtt) {
-        printf("Failed to inizialize MQTT client \n");
-        return 1;
-    }
-    // MQTT CLIENT INFO
-    mqtt->mqtt_client_info.client_id = MQTT_CLIENT_ID;
-    mqtt->mqtt_client_info.keep_alive = 60; // Keep alive in sec
+    static MQTT_CLIENT_DATA_T state;
+
+    // Use board unique id for the client id
+    static char client_id_buf[PICO_UNIQUE_BOARD_ID_SIZE_BYTES * 2 + 1];
+    pico_get_unique_board_id_string(client_id_buf, sizeof(client_id_buf));
+    state.mqtt_client_info.client_id = client_id_buf;
+    state.mqtt_client_info.keep_alive = 60; // Keep alive in sec
 
     if (cyw43_arch_init()) {
-        printf("Failed to inizialize CYW43\n");
-        return 1;
+        panic("Failed to inizialize CYW43");
     }
 
     cyw43_arch_enable_sta_mode();
-    if (cyw43_arch_wifi_connect_timeout_ms(WIFI_SSID, WIFI_PASSWORD, CYW43_AUTH_WPA2_AES_PSK, 10000)) {
-        printf("Wi-Fi error\n");
-        return 1;
+    if (cyw43_arch_wifi_connect_timeout_ms(WIFI_SSID, WIFI_PASSWORD, CYW43_AUTH_WPA2_AES_PSK, 30000)) {
+        panic("Failed to connect");
     }
     printf("\nConnected to Wifi\n");
 
-    ip_addr_t addr;
-    if (!ip4addr_aton(MQTT_BROKER_IP, &addr)) {
-        printf("MQTT ip Address not valid !\n");
-        return 1;
+    // We are not in a callback so locking is needed when calling lwip
+    // Make a DNS request for the MQTT server IP address
+    cyw43_arch_lwip_begin();
+    int err = dns_gethostbyname(MQTT_SERVER, &state.mqtt_server_address, dns_found, &state);
+    cyw43_arch_lwip_end();
+
+    if (err == ERR_OK) {
+        // We have the address, just start the client
+        start_client(&state);
+    } else if (err != ERR_INPROGRESS) { // ERR_INPROGRESS means expect a callback
+        panic("dns request failed");
     }
 
-    mqtt->mqtt_client_inst = mqtt_client_new();
-    if (!mqtt->mqtt_client_inst) {
-        printf("MQTT client instance creation error\n");
-        return 1;
-    }
-
-    if (mqtt_client_connect(mqtt->mqtt_client_inst, &addr, PORT, mqtt_connection_cb, mqtt, &mqtt->mqtt_client_info) != ERR_OK) {
-        printf("MQTT broker connection error\n");
-        return 1;
-    }
-    printf("Successfully connected to the MQTT broker\n");
-    mqtt_set_inpub_callback(mqtt->mqtt_client_inst, mqtt_incoming_publish_cb, mqtt_incoming_data_cb, mqtt);
-
-    while (1) {
-        publish_temperature(); // Public temperature every 5 sec
-        sleep_ms(5000);
-        tight_loop_contents();
+    while (true) {
+        cyw43_arch_poll();
+        cyw43_arch_wait_for_work_until(make_timeout_time_ms(10000));
     }
 
     return 0;
