10150677e3e2041d138e509f03f46384354e8e7d
Hiroyuki OYAMA <oyama@module.jp>
2025-03-19 17:12:48 +0900

    Rename structs, specify partition family, remove magic numbers

--- flash/partition_info/partition_info.c
+++ flash/partition_info/partition_info.c
@@ -1,5 +1,4 @@
 /**
- * Copyright (c) 2025 Hiroyuki OYAMA <oyama@module.jp>
  * Copyright (c) 2023 Raspberry Pi (Trading) Ltd.
  *
  * SPDX-License-Identifier: BSD-3-Clause
@@ -12,23 +11,28 @@
 
 #define PART_LOC_FIRST(x) ( ((x) & PICOBIN_PARTITION_LOCATION_FIRST_SECTOR_BITS) >> PICOBIN_PARTITION_LOCATION_FIRST_SECTOR_LSB )
 #define PART_LOC_LAST(x)  ( ((x) & PICOBIN_PARTITION_LOCATION_LAST_SECTOR_BITS)  >> PICOBIN_PARTITION_LOCATION_LAST_SECTOR_LSB )
+#define WORD_PADDED_BYTES(x)  ((x + 3) & ~3)
 
 #define WORD_SIZE  4
+#define PARTITION_LOCATION_AND_FLAGS_SIZE  2
+#define PARTITION_ID_SIZE                  2
+#define PARTITION_NAME_MAX                 127
+#define PARTITION_SIZE_MAX  (PARTITION_LOCATION_AND_FLAGS_SIZE + PARTITION_ID_SIZE + PARTITION_NAME_MAX + 1)
 
 /*
  * Stores partition table information and data read status
  */
 typedef struct {
-    int flags;
-    uint32_t *table;
+    uint32_t workarea[4 + PARTITION_TABLE_MAX_PARTITIONS * PARTITION_SIZE_MAX];
     size_t table_size;
+    uint32_t fields;
     int partition_count;
     uint32_t unpartitioned_space_first_sector;
     uint32_t unpartitioned_space_last_sector;
     uint32_t permission;
     int current_partition;
     size_t idx;
-} pico_partition_t;
+} pico_partition_table_t;
 
 /*
  * Stores information on each partition
@@ -38,8 +42,8 @@ typedef struct {
     uint32_t last_sector;
     uint32_t permission;
     uint64_t partition_id;
-    char name[127 + 1];  // name length is indicated by 7 bits
-} pico_partition_entry_t;
+    char name[PARTITION_NAME_MAX + 1];  // name length is indicated by 7 bits
+} pico_partition_t;
 
 
 /*
@@ -47,21 +51,21 @@ typedef struct {
  *
  * See the RP2350 datasheet 5.1.2, 5.4.8.16 for flags and structures that can be specified.
  */
-int pico_partitions_open(pico_partition_t *pt, int flags) {
-    pt->flags = flags;
-    static uint32_t workarea[816];
-    int rc = rom_get_partition_table_info(workarea, sizeof(workarea), flags);
+int pico_partitions_open(pico_partition_table_t *pt, int flags) {
+    int rc = rom_get_partition_table_info(pt->workarea, sizeof(pt->workarea), flags);
     if (rc < 0) {
         return rc;
     }
-    pt->table = workarea;
     pt->table_size = rc * WORD_SIZE;  // word to bytes
-    uint32_t location = workarea[2];
+
+    size_t pos = 0;
+    pt->fields = pt->workarea[pos++];
+    pt->partition_count = pt->workarea[pos++] & 0x000000FF;
+    uint32_t location = pt->workarea[pos++];
     pt->unpartitioned_space_first_sector = PART_LOC_FIRST(location);
     pt->unpartitioned_space_last_sector = PART_LOC_LAST(location);
-    pt->permission = workarea[3];
-    pt->partition_count = ((uint8_t*)workarea)[4];
-    pt->idx = 4;  // point to the beggining of eath table
+    pt->permission = pt->workarea[pos++];
+    pt->idx = pos;  // point to the beggining of eath partition
     pt->current_partition = 0;
 
     return 0;
@@ -70,37 +74,34 @@ int pico_partitions_open(pico_partition_t *pt, int flags) {
 /*
  * Parse a partition table and extract information
  */
-size_t pico_partitions_parse(pico_partition_t *pt, pico_partition_entry_t *p) {
+size_t pico_partitions_parse(pico_partition_table_t *pt, pico_partition_t *p) {
     size_t idx = pt->idx;
-    if (pt->flags & PT_INFO_PARTITION_LOCATION_AND_FLAGS) {
-        uint32_t location = pt->table[idx++];
+    if (pt->fields & PT_INFO_PARTITION_LOCATION_AND_FLAGS) {
+        uint32_t location = pt->workarea[idx++];
         p->first_sector = PART_LOC_FIRST(location);
         p->last_sector  = PART_LOC_LAST(location);
-        p->permission   = pt->table[idx++];
+        p->permission   = pt->workarea[idx++];
     } else {
         p->first_sector = 0;
         p->last_sector = 0;
         p->permission = 0;
     }
 
-    if (pt->flags & PT_INFO_PARTITION_ID) {
-        uint32_t id_low  = pt->table[idx++];
-        uint32_t id_high = pt->table[idx++];
+    if (pt->fields & PT_INFO_PARTITION_ID) {
+        uint32_t id_low  = pt->workarea[idx++];
+        uint32_t id_high = pt->workarea[idx++];
         p->partition_id = ((uint64_t)id_high << 32) | id_low;
     } else {
         p->partition_id = 0;
     }
 
-    if (pt->flags & PT_INFO_PARTITION_NAME) {
-        uint8_t *name_field = (uint8_t *)&pt->table[idx];
+    if (pt->fields & PT_INFO_PARTITION_NAME) {
+        uint8_t *name_field = (uint8_t *)&pt->workarea[idx];
         uint8_t name_length = name_field[0];
-        uint8_t *name_src = name_field + 1;
-        memcpy(p->name, name_src, name_length);
+        memcpy(p->name, name_field + 1, name_length);
         p->name[name_length] = '\0';
         size_t total_name_field_length = 1 + name_length;
-        // Name field is padded to word size
-        size_t padded_bytes = (total_name_field_length + 3) & ~((size_t)3);
-        idx += padded_bytes / WORD_SIZE;
+        idx += WORD_PADDED_BYTES(total_name_field_length) / WORD_SIZE;
     } else {
         p->name[0] = '\0';
     }
@@ -110,7 +111,7 @@ size_t pico_partitions_parse(pico_partition_t *pt, pico_partition_entry_t *p) {
 /*
  * Extract one partition information
  */
-bool pico_partitions_next(pico_partition_t *pt, pico_partition_entry_t *p) {
+bool pico_partitions_next(pico_partition_table_t *pt, pico_partition_t *p) {
     if (pt->current_partition >= pt->partition_count) {
         return false;
     }
@@ -122,7 +123,7 @@ bool pico_partitions_next(pico_partition_t *pt, pico_partition_entry_t *p) {
 int main() {
     stdio_init_all();
 
-    pico_partition_t pt;
+    pico_partition_table_t pt;
     pico_partitions_open(&pt, PT_INFO_PT_INFO | PT_INFO_PARTITION_LOCATION_AND_FLAGS |
                               PT_INFO_PARTITION_ID | PT_INFO_PARTITION_NAME);
     printf("un-partitioned_space: S(%s%s) NSBOOT(%s%s) NS(%s%s)\n",
@@ -133,10 +134,10 @@ int main() {
            (pt.permission & PICOBIN_PARTITION_PERMISSION_NS_R_BITS ? "r" : ""),
            (pt.permission & PICOBIN_PARTITION_PERMISSION_NS_W_BITS ? "w" : ""));
     printf("patitions:\n");
-    pico_partition_entry_t p;
+    pico_partition_t p;
     while (pico_partitions_next(&pt, &p)) {
         printf("%3d:", pt.current_partition - 1);
-        if (pt.flags & PT_INFO_PARTITION_LOCATION_AND_FLAGS) {
+        if (pt.fields & PT_INFO_PARTITION_LOCATION_AND_FLAGS) {
             printf("    %08x->%08x S(%s%s) NSBOOT(%s%s) NS(%s%s)",
                    p.first_sector * 4096, p.last_sector * 4096,
                    (p.permission & PICOBIN_PARTITION_PERMISSION_S_R_BITS ? "r" : ""),
@@ -145,12 +146,11 @@ int main() {
                    (p.permission & PICOBIN_PARTITION_PERMISSION_NSBOOT_W_BITS ? "w" : ""),
                    (p.permission & PICOBIN_PARTITION_PERMISSION_NS_R_BITS ? "r" : ""),
                    (p.permission & PICOBIN_PARTITION_PERMISSION_NS_W_BITS ? "w" : ""));
-
         }
-        if (pt.flags & PT_INFO_PARTITION_ID) {
+        if (pt.fields & PT_INFO_PARTITION_ID) {
             printf(", id=%016llx", p.partition_id);
         }
-        if (pt.flags & PT_INFO_PARTITION_NAME) {
+        if (pt.fields & PT_INFO_PARTITION_NAME) {
             printf(", \"%s\"", p.name);
         }
         printf("\n");
