9bdba25764534f0c760d66e16c81b76641c71862
Hiroyuki OYAMA <oyama@module.jp>
2025-03-22 01:35:24 +0900

    remove WORD_SIZE
    Co-authored-by: will-v-pi <108662275+will-v-pi@users.noreply.github.com>

--- flash/partition_info/partition_info.c
+++ flash/partition_info/partition_info.c
@@ -101,7 +101,7 @@ bool pico_partitions_next(pico_partition_table_t *pt, pico_partition_t *p) {
 
     if (p->flags_and_permissions & PICOBIN_PARTITION_FLAGS_HAS_NAME_BITS) {
         // Read variable length fields
-        uint32_t name_buf[(PARTITION_NAME_MAX + 1) / WORD_SIZE] = {0};
+        uint32_t name_buf[(PARTITION_NAME_MAX + 1) / sizeof(uint32_t)] = {0};
         int rc = rom_get_partition_table_info(name_buf, sizeof(name_buf),
                                               (pt->current_partition << 24 |
                                                PT_INFO_SINGLE_PARTITION |
