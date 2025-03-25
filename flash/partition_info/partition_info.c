ee6d8d6998427f365eb27193e2bace77142a810c
Hiroyuki OYAMA <oyama@module.jp>
2025-03-25 13:06:02 +0900

    Unify pico-sdk header file includes to ""

--- flash/partition_info/partition_info.c
+++ flash/partition_info/partition_info.c
@@ -4,8 +4,8 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 #include <stdio.h>
-#include <pico/stdlib.h>
-#include <pico/bootrom.h>
+#include "pico/stdlib.h"
+#include "pico/bootrom.h"
 #include "boot/picobin.h"
 
 
