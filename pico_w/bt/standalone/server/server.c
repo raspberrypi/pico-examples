efd6100294e8682aa240b0a72ff429c80dad4450
Peter Harper <peter.harper@raspberrypi.com>
2025-07-07 18:03:11 +0100

    Simplify standaline BT examples
    Move client and server to their own folder
    Get rid of the wifi stuff

--- pico_w/bt/standalone/server/server.c
+++ pico_w/bt/standalone/server/server.c
@@ -6,12 +6,17 @@
 
 #include <stdio.h>
 #include "btstack.h"
+#include "pico/cyw43_arch.h"
+#include "pico/btstack_cyw43.h"
 #include "hardware/adc.h"
+#include "pico/stdlib.h"
 
 #include "temp_sensor.h"
-#include "server_common.h"
 
+#define HEARTBEAT_PERIOD_MS 1000
+#define ADC_CHANNEL_TEMPSENSOR 4
 #define APP_AD_FLAGS 0x06
+
 static uint8_t adv_data[] = {
     // Flags general discoverable
     0x02, BLUETOOTH_DATA_TYPE_FLAGS, APP_AD_FLAGS,
@@ -24,6 +29,10 @@ static const uint8_t adv_data_len = sizeof(adv_data);
 int le_notification_enabled;
 hci_con_handle_t con_handle;
 uint16_t current_temp;
+static btstack_timer_source_t heartbeat;
+static btstack_packet_callback_registration_t hci_event_callback_registration;
+
+extern uint8_t const profile_data[];
 
 void packet_handler(uint8_t packet_type, uint16_t channel, uint8_t *packet, uint16_t size) {
     UNUSED(size);
@@ -76,7 +85,7 @@ int att_write_callback(hci_con_handle_t connection_handle, uint16_t att_handle, 
     UNUSED(transaction_mode);
     UNUSED(offset);
     UNUSED(buffer_size);
-    
+
     if (att_handle != ATT_CHARACTERISTIC_ORG_BLUETOOTH_CHARACTERISTIC_TEMPERATURE_01_CLIENT_CONFIGURATION_HANDLE) return 0;
     le_notification_enabled = little_endian_read_16(buffer, 0) == GATT_CLIENT_CHARACTERISTICS_CONFIGURATION_NOTIFICATION;
     con_handle = connection_handle;
@@ -97,10 +106,96 @@ void poll_temp(void) {
     // ref https://github.com/raspberrypi/pico-micropython-examples/blob/master/adc/temperature.py
     const float conversion_factor = 3.3 / (65535);
     float reading = raw16 * conversion_factor;
-    
+
     // The temperature sensor measures the Vbe voltage of a biased bipolar diode, connected to the fifth ADC channel
-    // Typically, Vbe = 0.706V at 27 degrees C, with a slope of -1.721mV (0.001721) per degree. 
+    // Typically, Vbe = 0.706V at 27 degrees C, with a slope of -1.721mV (0.001721) per degree.
     float deg_c = 27 - (reading - 0.706) / 0.001721;
     current_temp = deg_c * 100;
     printf("Write temp %.2f degc\n", deg_c);
  }
+
+static void heartbeat_handler(struct btstack_timer_source *ts) {
+    static uint32_t counter = 0;
+    counter++;
+
+    // Update the temp every 10s
+    if (counter % 10 == 0) {
+        poll_temp();
+        if (le_notification_enabled) {
+            att_server_request_can_send_now_event(con_handle);
+        }
+    }
+
+    // Invert the led
+    static int led_on = true;
+    led_on = !led_on;
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, led_on);
+
+    // Restart timer
+    btstack_run_loop_set_timer(ts, HEARTBEAT_PERIOD_MS);
+    btstack_run_loop_add_timer(ts);
+}
+
+static volatile bool key_pressed;
+void key_pressed_func(void *param) {
+    int key = getchar_timeout_us(0); // get any pending key press but don't wait
+    if (key == 's' || key == 'S') {
+        key_pressed = true;
+    }
+}
+
+int main() {
+    stdio_init_all();
+
+restart:
+    // initialize CYW43 driver architecture (will enable BT if/because CYW43_ENABLE_BLUETOOTH == 1)
+    if (cyw43_arch_init()) {
+        printf("failed to initialise cyw43_arch\n");
+        return -1;
+    }
+
+    // Get notified if the user presses a key
+    printf("Press the \"S\" key to Stop bluetooth\n");
+    stdio_set_chars_available_callback(key_pressed_func, NULL);
+
+    // Initialise adc for the temp sensor
+    adc_init();
+    adc_select_input(ADC_CHANNEL_TEMPSENSOR);
+    adc_set_temp_sensor_enabled(true);
+
+    l2cap_init();
+    sm_init();
+
+    att_server_init(profile_data, att_read_callback, att_write_callback);
+
+    // inform about BTstack state
+    hci_event_callback_registration.callback = &packet_handler;
+    hci_add_event_handler(&hci_event_callback_registration);
+
+    // register for ATT event
+    att_server_register_packet_handler(packet_handler);
+
+    // set one-shot btstack timer
+    heartbeat.process = &heartbeat_handler;
+    btstack_run_loop_set_timer(&heartbeat, HEARTBEAT_PERIOD_MS);
+    btstack_run_loop_add_timer(&heartbeat);
+
+    // turn on bluetooth!
+    hci_power_control(HCI_POWER_ON);
+
+    key_pressed = false;
+    while(!key_pressed) {
+        async_context_poll(cyw43_arch_async_context());
+        async_context_wait_for_work_until(cyw43_arch_async_context(), at_the_end_of_time);
+    }
+
+    cyw43_arch_deinit();
+
+    printf("Press the \"S\" key to Start bluetooth\n");
+    key_pressed = false;
+    while(!key_pressed) {
+        sleep_ms(1000);
+    }
+    goto restart;
+    return 0;
+}
