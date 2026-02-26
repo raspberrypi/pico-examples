23afab6450bb0330b634e1da09b5bb6cc3913e3c
Martin Crossley <dev@crossleys.biz>
2026-02-26 15:48:56 +0000

    use status_led library, improve comments

--- async_context/simple_at_time_worker/simple_at_time_worker.c
+++ async_context/simple_at_time_worker/simple_at_time_worker.c
@@ -1,89 +1,69 @@
 /**
- *  Copyright (c) 2025 mjcross
+ *  Copyright (c) 2026 mjcross
  * 
  *  SPDX-License-Identifier: BSD-3-Clause
  */
 
 #include "pico/stdlib.h"
 #include "pico/async_context_threadsafe_background.h"
-
-// for Pico W devices the on-board LED is controlled via the WiFi module
-#ifdef CYW43_WL_GPIO_LED_PIN
-#include "pico/cyw43_arch.h"
-#endif
+#include "pico/status_led.h"
 
 #ifndef LED_DELAY_MS
 #define LED_DELAY_MS 500
 #endif
 
-// generic function to initialise the on-board LED
-int pico_led_init(void) {
-#if defined(PICO_DEFAULT_LED_PIN)
-    gpio_init(PICO_DEFAULT_LED_PIN);
-    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);   // non-WiFi boards
-    return PICO_OK;
-#elif defined(CYW43_WL_GPIO_LED_PIN)
-    return cyw43_arch_init();   // WiFi boards
-#endif
-}
-
-// generic function to turn the on-board LED on or off
-void pico_led_set(bool state) {
-#if defined(PICO_DEFAULT_LED_PIN)
-    gpio_put(PICO_DEFAULT_LED_PIN, state); // non-WiFi boards
-#elif defined(CYW43_WL_GPIO_LED_PIN)
-    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, state); // WiFi boards
-#endif    
-}
-
-// a simple user data structure for our async at-time worker
+// define a user data type to be passed to our worker's callback function
 typedef struct {
-    bool state;
-} led_state_t;
+    bool led_state;
+} my_data_t;
 
-// callback function for our async at-time worker
-// note: this function MUST be safe to call from an IRQ
+
+// define a callback function for our worker (MUST be safe to call from an IRQ)
 void worker_cb(async_context_t *p_ctx, async_at_time_worker_t *p_worker) {
-    // read user data from worker
-    led_state_t *p_led = (led_state_t *)(p_worker->user_data);
+    // unpack our user data instance from `(void *)worker.user_data`
+    my_data_t *p_my_data = p_worker->user_data;
 
-    // toggle the LED
-    p_led->state = !p_led->state;
-    pico_led_set(p_led->state);
+    // update the status LED from our user data and toggle it
+    status_led_set_state(p_my_data->led_state);
+    p_my_data->led_state = !p_my_data->led_state;
 
-    // at-time workers are automatically removed from the context just before they run
-    // so to keep the LED flashing we must now re-schedule the task
+    // re-schedule the worker, so as to flash the LED continuously (an at_time worker is 
+    // automatically removed from the context just before it runs)
     async_context_add_at_time_worker_in_ms(p_ctx, p_worker, LED_DELAY_MS);
 }
 
+// create an instance of our user data type (MUST still exist when the worker runs)
+static my_data_t my_data = {
+    .led_state = true 
+};
+
+// create an asynchronous at-time worker that points to our callback function and user
+// data (MUST still exist when the worker runs)
+static async_at_time_worker_t worker = {
+    .do_work = worker_cb,
+    .user_data = &my_data
+};
+
 
 int main() {
-    // initialise the LED and our user data structure
+    // initialise the status LED
     hard_assert(
-        pico_led_init() == PICO_OK
-    );
-    led_state_t led_state = {
-        .state = false
-    };
-
-    // create and initialise an async background context
-    // note: in a networking application we might typically use the context returned by
-    // cyw43_arch_async_context() instead of creating a new one here
-    async_context_threadsafe_background_t ctx;
-    hard_assert(
-        async_context_threadsafe_background_init_with_defaults(&ctx) == true
+        status_led_init() == true
     );
 
-    // define an async at-time worker that will run our callback function
-    async_at_time_worker_t worker = {
-        .do_work = worker_cb,
-        .user_data = &led_state
-    };
-
-    // add an at-time worker to the context, scheduled to run after LED_DELAY_MS
-    // note: ctx.core is the underlying async_context_t of our threadsafe background
+    // create and initialise an asynchronous background context with default settings
+    // note: in a networking application we'd probably reuse the existing cyw43_arch
+    // context directly with `async_context_t ctx = cyw43_arch_async_context()`
+    async_context_threadsafe_background_t background;
     hard_assert(
-        async_context_add_at_time_worker_in_ms(&ctx.core, &worker, LED_DELAY_MS) == true
+        async_context_threadsafe_background_init_with_defaults(&background) == true
+    );
+
+    // add our at-time worker to the background context, to run after LED_DELAY_MS
+    // note: `background.core` is the async_context_t of our threadsafe background. If
+    // we were using the cyw43 context instead (see above) we'd just put `&ctx` here.
+    hard_assert(
+        async_context_add_at_time_worker_in_ms(&background.core, &worker, LED_DELAY_MS) == true
     );
 
     // the LED will flash in the background 
