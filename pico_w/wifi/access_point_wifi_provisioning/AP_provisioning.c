cc771f27b3d594175fd6ec219c8aa555a470ad5b
Peter Harper <peter.harper@raspberrypi.com>
2026-06-25 20:20:35 +0100

    Rework wifi connection

--- pico_w/wifi/access_point_wifi_provisioning/AP_provisioning.c
+++ pico_w/wifi/access_point_wifi_provisioning/AP_provisioning.c
@@ -22,10 +22,7 @@ static absolute_time_t wifi_connected_time;
 // max lengths + 1
 static char ssid[33];
 static char password[64];
-
 static int num_credentials;
-
-static bool connection_status = false;
 
 // how many sectors would you like to reserve
 // each sector is 4096 bytes, so can hold 40 pairs of max length credentials
@@ -82,6 +79,14 @@ static const char *ssi_tags[] = {
     "password"
 };
 
+#define CYW43_STA_IS_ACTIVE(self) (((self)->itf_state >> CYW43_ITF_STA) & 1)
+#define CYW43_AP_IS_ACTIVE(self) (((self)->itf_state >> CYW43_ITF_AP) & 1)
+
+#define WIFI_CONNECT_TIME_S 20
+
+#define AP_SSID "picow_test"
+#define AP_PASSWORD "password"
+
 int main() {
     stdio_init_all();
     if (cyw43_arch_init()) {
@@ -90,72 +95,86 @@ int main() {
     }
     printf("intitialised\n");
 
-    // for testing, erase memory first
-    // might need to erase memory first time you use provisioning in case there are garbage values in flash
-    //int rc = flash_safe_execute(call_flash_range_erase, (void*)FLASH_TARGET_OFFSET, UINT32_MAX);
-    //hard_assert(rc == PICO_OK);
+    printf("Press 'w' in the next 3s to wipe stored ssid and passwords\n");
+    int c = getchar_timeout_us(3000000);
+    if (c == 'w' || c == 'W') {
+        // for testing, erase memory first
+        // might need to erase memory first time you use provisioning in case there are garbage values in flash
+        int rc = flash_safe_execute(call_flash_range_erase, (void*)FLASH_TARGET_OFFSET, UINT32_MAX);
+        hard_assert(rc == PICO_OK);
+    }
 
     // First, try to connect to network using saved credentials
     read_credentials();
 
-    cyw43_arch_enable_sta_mode();
     for (int i = 0; i < num_credentials; i++) {
-        if (cyw43_arch_wifi_connect_timeout_ms(ssid_list[i], password_list[i], CYW43_AUTH_WPA2_AES_PSK, 5000)) { 
-            printf("failed to connect with saved credentials %i \n", i);
+        if (i == 0) cyw43_arch_enable_sta_mode();
+        printf("Trying to connect with STA \"%s\" at index %d\n", ssid_list[i], i);
+        int rc = cyw43_arch_wifi_connect_timeout_ms(ssid_list[i], password_list[i], CYW43_AUTH_WPA2_AES_PSK, WIFI_CONNECT_TIME_S * 1000);
+        if (rc) { 
+            printf("failed to connect with saved credentials %d\n", rc);
+            cyw43_arch_disable_sta_mode();
         } else {
             printf("Connected.\n");
-            connection_status = true;
             break;
         }
     }
 
-    // If this fails, enable access point
-    if (connection_status == false) {
-        cyw43_arch_disable_sta_mode();
-        cyw43_arch_enable_ap_mode("picow_test", "password", CYW43_AUTH_WPA2_AES_PSK);
-        printf("\nReady, running server at %s\n", ip4addr_ntoa(netif_ip4_addr(netif_list)));
+    while(cyw43_wifi_link_status(&cyw43_state, CYW43_ITF_STA) != CYW43_LINK_JOIN) {
+        // If STA is not active, enable access point
+        if (!CYW43_STA_IS_ACTIVE(&cyw43_state) && !CYW43_AP_IS_ACTIVE(&cyw43_state)) {
+            cyw43_arch_enable_ap_mode(AP_SSID, AP_PASSWORD, CYW43_AUTH_WPA2_AES_PSK);
+            printf("\nReady, running web server at %s\n", ip4addr_ntoa(netif_ip4_addr(netif_list)));
+            printf("Connect to ssid \"%s\" with password \"%s\"\n", AP_SSID, AP_PASSWORD);
 
-        #if LWIP_IPV6
-        #define IP(x) ((x).u_addr.ip4)
-        #else
-        #define IP(x) (x)
-        #endif
+            #if LWIP_IPV6
+            #define IP(x) ((x).u_addr.ip4)
+            #else
+            #define IP(x) (x)
+            #endif
 
-        ip4_addr_t mask;
-        ip4_addr_t gw;
-        IP(gw).addr = PP_HTONL(CYW43_DEFAULT_IP_AP_ADDRESS);
-        IP(mask).addr = PP_HTONL(CYW43_DEFAULT_IP_MASK);
+            ip4_addr_t mask;
+            ip4_addr_t gw;
+            IP(gw).addr = PP_HTONL(CYW43_DEFAULT_IP_AP_ADDRESS);
+            IP(mask).addr = PP_HTONL(CYW43_DEFAULT_IP_MASK);
 
-        #undef IP
-        dhcp_server_t dhcp_server;
-        dhcp_server_init(&dhcp_server, &gw, &mask);
+            #undef IP
+            dhcp_server_t dhcp_server;
+            dhcp_server_init(&dhcp_server, &gw, &mask);
 
-        dns_server_t dns_server;
-        dns_server_init(&dns_server, &gw);
+            dns_server_t dns_server;
+            dns_server_init(&dns_server, &gw);
 
-        char hostname[sizeof(CYW43_HOST_NAME) + 4];
-        memcpy(&hostname[0], CYW43_HOST_NAME, sizeof(CYW43_HOST_NAME) - 1);
-        hostname[sizeof(hostname) - 1] = '\0';
-        netif_set_hostname(&cyw43_state.netif[CYW43_ITF_STA], hostname);
+            char hostname[sizeof(CYW43_HOST_NAME) + 4];
+            memcpy(&hostname[0], CYW43_HOST_NAME, sizeof(CYW43_HOST_NAME) - 1);
+            hostname[sizeof(hostname) - 1] = '\0';
+            netif_set_hostname(&cyw43_state.netif[CYW43_ITF_STA], hostname);
 
-        // start http server
-        wifi_connected_time = get_absolute_time();
+            // start http server
+            wifi_connected_time = get_absolute_time();
 
-        // setup http server
-        cyw43_arch_lwip_begin();
-        httpd_init();
-        http_set_cgi_handlers(cgi_handlers, LWIP_ARRAYSIZE(cgi_handlers));
-        http_set_ssi_handler(ssi_handler, ssi_tags, LWIP_ARRAYSIZE(ssi_tags));
-        cyw43_arch_lwip_end();
-    }
-
-    //wait for connection
-    while(connection_status == false) {
+            // setup http server
+            cyw43_arch_lwip_begin();
+            httpd_init();
+            http_set_cgi_handlers(cgi_handlers, LWIP_ARRAYSIZE(cgi_handlers));
+            http_set_ssi_handler(ssi_handler, ssi_tags, LWIP_ARRAYSIZE(ssi_tags));
+            cyw43_arch_lwip_end();
+        } else if (CYW43_STA_IS_ACTIVE(&cyw43_state) && cyw43_wifi_link_status(&cyw43_state, CYW43_ITF_STA) == CYW43_LINK_DOWN) {
+            cyw43_arch_disable_ap_mode();
+            printf("Trying to connect with STA \"%s\"\n", ssid);
+            int rc = cyw43_arch_wifi_connect_timeout_ms(ssid, password, CYW43_AUTH_WPA2_AES_PSK, WIFI_CONNECT_TIME_S * 1000);
+            if (rc) { 
+                printf("failed to connect with saved credentials %d\n", rc);
+                cyw43_arch_disable_sta_mode();
+            } else {
+                printf("Connected.\n");
+                break;
+            }
+        }
         cyw43_arch_poll();
         cyw43_arch_wait_for_work_until(make_timeout_time_ms(1000));
     }
-
-    printf("Finished provisioning credentials. \n");
+    printf("Finished provisioning credentials.\n");
     cyw43_arch_deinit();
     return 0;
 }
@@ -182,18 +201,18 @@ static void save_credentials(char ssid[], char password[]) {
     uint password_len = strlen(password);
     uint credential_count;
 
-    //first check how many credentials are already saved
+    // first check how many credentials are already saved
     if (flash_target_contents[1] != 255) {
         credential_count = flash_target_contents[1];
-        //incriment this count since we are about to add a credential
+        // increment this count since we are about to add a credential
         credential_count++;
         flash_data[1] = credential_count;
     } else {
-        // first (empty) save, so dont want to incriment
+        // first (empty) save, so dont want to increment
         credential_count = 0;
     }
 
-    //now need to find how far through the flash to start writing, and also add previous stuff to flash data
+    // now need to find how far through the flash to start writing, and also add previous stuff to flash data
     uint write_start_location = 2;
     if (credential_count != 0) {
         uint count = 0;
@@ -210,11 +229,10 @@ static void save_credentials(char ssid[], char password[]) {
     // first add ssid 
     for (uint i = 0; i < ssid_len; i++) {
         int ascii = (int) ssid[i];
-        //printf("%i\n", ascii);
         flash_data[i + write_start_location] = ascii;
     }
 
-    //next add password
+    // next add password
     for (uint i = 0; i < password_len; i++) {
         int ascii = (int) password[i];
         flash_data[i + ssid_len + write_start_location + 1] = ascii;
@@ -239,10 +257,12 @@ static void read_credentials(void) {
         save_credentials("", "");
     }
 
-    //second byte saves credential count (allows 255 sets of credentials, should be enough)
+    // second byte saves credential count (allows 255 sets of credentials, should be enough)
     credential_count = flash_target_contents[1];
     num_credentials = credential_count;
-    //initialise temporary ssid and password as 1 bigger than max to ensure null termination
+    printf("read_credentials %d\n", num_credentials);
+
+    // initialise temporary ssid and password as 1 bigger than max to ensure null termination
     char t_ssid_list[20][33] = {0};
     char t_password_list[20][64] = {0};
 
@@ -255,16 +275,12 @@ static void read_credentials(void) {
         } else if (flash_target_contents[i] == 0) {
             space_count++;
             start_index = i;
-            printf("\n");
-            //printf("space count %i\n", space_count);
         } else if (flash_target_contents[i] != 0 && space_count % 2 == 0) {
             // there is a char, and even space count. So we are reading a ssid
             t_ssid_list[(int) space_count / 2][i - start_index - 1] = flash_target_contents[i];
-            //printf("%c", flash_target_contents[i]);
         } else if (flash_target_contents[i] != 0 && space_count % 2 == 1) {
             // there is a char and odd space count, so reading password
             t_password_list[(int) space_count / 2][i - start_index - 1] = flash_target_contents[i];
-            //printf("%c", flash_target_contents[i]);
         } 
     }
     
@@ -277,48 +293,132 @@ static void read_credentials(void) {
 }
 
 static void attempt_wifi_connection(void) {
-    cyw43_arch_disable_ap_mode();
-    cyw43_arch_enable_sta_mode();
+    save_credentials(ssid, password);
+    read_credentials();
+    cyw43_arch_enable_sta_mode(); // should trigger a connection attempt
+}
 
-    if (cyw43_arch_wifi_connect_timeout_ms(ssid, password, CYW43_AUTH_WPA2_AES_PSK, 15000)) { 
-        panic("Failed to connect!");
-    } else {
-        printf("Connected.\n");
-        connection_status = true;
-        // success, so save credentials for future use
-        save_credentials(ssid, password);
+// Decodes application/x-www-form-urlencoded text in place.
+// '+' -> ' ', "%XX" -> byte. dst and src may be the same buffer.
+// Trailing spaces are stripped from the result.
+static void url_decode(char *dst, const char *src, size_t dst_size) {
+    size_t di = 0;
+    while (*src && di + 1 < dst_size) {
+        char c = *src;
+        if (c == '+') {
+            dst[di++] = ' ';
+            src++;
+        } else if (c == '%' && isxdigit((unsigned char)src[1]) && isxdigit((unsigned char)src[2])) {
+            int hi = src[1] <= '9' ? src[1] - '0' : (src[1] | 0x20) - 'a' + 10;
+            int lo = src[2] <= '9' ? src[2] - '0' : (src[2] | 0x20) - 'a' + 10;
+            dst[di++] = (char)((hi << 4) | lo);
+            src += 3;
+        } else {
+            dst[di++] = c;
+            src++;
+        }
     }
+    // strip trailing spaces
+    while (di > 0 && dst[di - 1] == ' ') {
+        di--;
+    }
+    dst[di] = '\0';
 }
 
-static const char *credential_cgi_handler(int iIndex, int iNumParams, char *pcParam[], char *pcValue[]) {
+// Returns the value of the named CGI parameter, or NULL if not present.
+static const char *cgi_param(const char *name, int iNumParams, char *pcParam[], char *pcValue[]) {
+    for (int i = 0; i < iNumParams; i++) {
+        if (pcParam[i] != NULL && strcmp(pcParam[i], name) == 0) {
+            return pcValue[i];
+        }
+    }
+    return NULL;
+}
+
+static const char *credential_cgi_handler(__unused int iIndex, int iNumParams, char *pcParam[], char *pcValue[]) {
     printf("credential_cgi_handler called\n");
-    strncpy(ssid, pcValue[0], sizeof(ssid) - 1);
-    strncpy(password, pcValue[1], sizeof(password) - 1);
-    printf("SSID AND PASSWORD: %s %s \n", ssid, password);
+
+    const char *s = cgi_param("ssid", iNumParams, pcParam, pcValue);
+    const char *p = cgi_param("password", iNumParams, pcParam, pcValue);
+    if (s == NULL || p == NULL) {
+        printf("missing ssid/password\n");
+        return "/index.shtml";
+    }
+
+    url_decode(ssid, s, sizeof(ssid));
+    url_decode(password, p, sizeof(password));
+    printf("SSID AND PASSWORD: >%s< >%s<\n", ssid, password);
     return "/index.shtml";
 }
 
-static const char *connect_from_saved_cgi_handler(int iIndex, int iNumParams, char *pcParam[], char *pcValue[]) {
-    printf("load_from_saved_cgi_handler called\n");
-    strncpy(ssid, ssid_list[atoi(pcValue[0])], sizeof(ssid) - 1);
-    strncpy(password, password_list[atoi(pcValue[0])], sizeof(password) - 1);
+static const char *connect_from_saved_cgi_handler(__unused int iIndex, int iNumParams, char *pcParam[], char *pcValue[]) {
+    printf("connect_from_saved_cgi_handler called\n");
+
+    const char *index_str = cgi_param("index", iNumParams, pcParam, pcValue);
+    if (num_credentials == 0 || index_str == NULL) {
+        printf("no saved network selected\n");
+        return "/index.shtml";
+    }
+
+    int idx = atoi(index_str);
+    if (idx < 0 || idx >= num_credentials) {
+        printf("index %d out of range\n", idx);
+        return "/index.shtml";
+    }
+
+    strncpy(ssid, ssid_list[idx], sizeof(ssid) - 1);
+    ssid[sizeof(ssid) - 1] = '\0';
+    strncpy(password, password_list[idx], sizeof(password) - 1);
+    password[sizeof(password) - 1] = '\0';
+
     attempt_wifi_connection();
     return "/index.shtml";
 }
 
-static const char *connect_cgi_handler(int iIndex, int iNumParams, char *pcParam[], char *pcValue[]) {
+static const char *connect_cgi_handler(__unused int iIndex, __unused int iNumParams, __unused char *pcParam[], __unused char *pcValue[]) {
     printf("connect_cgi_handler called\n");
+
+    if (ssid[0] == 0) {
+        printf("no ssid set\n");
+        return "/index.shtml";
+    }
+
     attempt_wifi_connection();
     return "/index.shtml";
 }
 
-static const char *clear_cgi_handler(int iIndex, int iNumParams, char *pcParam[], char *pcValue[]) {
+static const char *clear_cgi_handler(__unused int iIndex, __unused int iNumParams, __unused char *pcParam[], __unused char *pcValue[]) {
     printf("clear_cgi_handler called\n");
     int rc = flash_safe_execute(call_flash_range_erase, (void*)FLASH_TARGET_OFFSET, UINT32_MAX);
     hard_assert(rc == PICO_OK);
     save_credentials("", "");
     read_credentials();
     return "/index.shtml";
+}
+
+// Appends s to dst with HTML special chars escaped. Writes at most dst_size-1
+// chars plus a null terminator. Returns number of chars written (excl. null).
+static int html_escape(char *dst, size_t dst_size, const char *s) {
+    size_t di = 0;
+    for (; *s && di + 1 < dst_size; s++) {
+        const char *rep;
+        switch (*s) {
+            case '&':  rep = "&amp;";  break;
+            case '<':  rep = "&lt;";   break;
+            case '>':  rep = "&gt;";   break;
+            case '"':  rep = "&quot;"; break;
+            case '\'': rep = "&#39;";  break;
+            default:
+                dst[di++] = *s;
+                continue;
+        }
+        size_t rlen = strlen(rep);
+        if (di + rlen >= dst_size) break;   // not enough room for the whole entity
+        memcpy(dst + di, rep, rlen);
+        di += rlen;
+    }
+    dst[di] = '\0';
+    return (int)di;
 }
 
 // Note that the buffer size is limited by LWIP_HTTPD_MAX_TAG_INSERT_LEN, so use LWIP_HTTPD_SSI_MULTIPART to return larger amounts of data
@@ -330,19 +430,31 @@ static u16_t ssi_handler(int iIndex, char *pcInsert, int iInsertLen
     int printed = 0;
     switch (iIndex) {
         case 0: { // wifilist
-            for (int i = 0; i < num_credentials; i++) {
-                printed += snprintf(pcInsert + printed, iInsertLen - printed, 
-                                "<li>SSID: %s,   Password: %s,   Index: %i</li>", 
-                                ssid_list[i], password_list[i], i);
+            if (num_credentials == 0) {
+                printed = snprintf(pcInsert, iInsertLen, "<p>No saved networks</p>");
+                break;
+            }
+            int i = current_tag_part;
+            char esc[sizeof(ssid_list[0]) * 6];   // worst case: every char -> &quot; (6 chars)
+            html_escape(esc, sizeof(esc), ssid_list[i]);
+            printed = snprintf(pcInsert, iInsertLen,
+                "<label><input type=\"radio\" name=\"index\" value=\"%i\"%s> SSID: %s</label><br>",
+                i, (i == 0 ? " checked" : ""), esc);
+            if (i + 1 < num_credentials) {
+                *next_tag_part = i + 1;
             }
             break;
         }
         case 1: { // ssid
-            printed = snprintf(pcInsert, iInsertLen, ssid);
-            break;         
+            char esc[sizeof(ssid) * 6];
+            html_escape(esc, sizeof(esc), ssid);
+            printed = snprintf(pcInsert, iInsertLen, "%s", esc);
+            break;
         }
         case 2: { // password
-            printed = snprintf(pcInsert, iInsertLen, password);
+            char esc[sizeof(password) * 6];
+            html_escape(esc, sizeof(esc), password);
+            printed = snprintf(pcInsert, iInsertLen, "%s", esc);
             break;
         }
         default: { // unknown tag
