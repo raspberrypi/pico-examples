54dd822b028fefccda0fcfd4eb12bc2d089cfb47
Peter Harper <77111776+peterharperuk@users.noreply.github.com>
2026-06-27 21:25:31 +0100

    Use new pio functions (#766)
    * Use new pio functions
    
    We twiddle input_sync_bypass directly in the pio spi example. Use the
    new pio_set_input_sync_bypass_with_mask64 instead.
    
    Run the loopback example at full speed and improve it so it's more
    obvious when the example doesn't work.
    
    * Apply suggestion
    
    * Update pio/spi/spi_loopback.c
    
    Co-authored-by: Andrew Scheller <lurch@durge.org>
    
    * Fix PASSED check
    
    ---------
    
    Co-authored-by: Andrew Scheller <lurch@durge.org>

--- pio/spi/spi_loopback.c
+++ pio/spi/spi_loopback.c
@@ -21,6 +21,9 @@
 
 #define BUF_SIZE 20
 
+static int spi_example_passes;
+static int spi_example_runs;
+
 void test(const pio_spi_inst_t *spi) {
     static uint8_t txbuf[BUF_SIZE];
     static uint8_t rxbuf[BUF_SIZE];
@@ -40,17 +43,23 @@ void test(const pio_spi_inst_t *spi) {
         printf(" %02x", (int) rxbuf[i]);
         mismatch = mismatch || rxbuf[i] != txbuf[i];
     }
-    if (mismatch)
+    if (mismatch) {
         printf("\nNope\n");
-    else
+    } else {
         printf("\nOK\n");
+        spi_example_passes++;
+    }
 }
 
 int main() {
     stdio_init_all();
-
+    printf("spi_loopback start\n");
     pio_spi_inst_t spi[2];
+#if 1
+    float clkdiv = 1; // as fast as possible!
+#else
     float clkdiv = 31.25f;  // 1 MHz @ 125 clk_sys
+#endif
 
     // pio_claim_free_sm_and_add_program_for_gpio_range finds a free pio and state machine for a program and sets the gpio base correctly
     const uint pin_base = MIN(PIN_SCK, MIN(PIN_MOSI, PIN_MISO));
@@ -60,6 +69,7 @@ int main() {
 
     for (int cpha = 0; cpha <= 1; ++cpha) {
         for (int cpol = 0; cpol <= 1; ++cpol) {
+            spi_example_runs++;
             printf("CPHA = %d, CPOL = %d\n", cpha, cpol);
             pio_spi_init(spi[cpha].pio, spi[cpha].sm,
                          spi[cpha].offset,
@@ -75,4 +85,6 @@ int main() {
             sleep_ms(10);
         }
     }
+    printf("Example %s\n", spi_example_passes == spi_example_runs ? "PASSED" : "FAILED");
+    assert(spi_example_passes == spi_example_runs);
 }
